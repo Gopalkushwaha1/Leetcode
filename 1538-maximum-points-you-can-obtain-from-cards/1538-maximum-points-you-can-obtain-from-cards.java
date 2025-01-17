e50db77796ead02250dbdf689a49f5f280a47f7b
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-17 14:58:56 +0530

    Time: 2 ms (99.27%), Space: 55 MB (75.18%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.java
@@ -3,23 +3,25 @@ class Solution {
         int n = cardPoints.length ; 
         int start = 0 ; 
         int end = 0 ; 
-        int max = 0 ;
+        int maxCardValue = 0 ;   // Variable to track max Card value for k card 
         
+        // Making Window of length k
         for ( end = 0 ; end< k ; end++) {
-            max += cardPoints[end] ; 
+            maxCardValue += cardPoints[end] ; 
         }
-        int localMax = max ;
+        int localMax = maxCardValue ;    // Variable to track local max Card value for k card 
 
 
-        int last = cardPoints.length - 1 ;
+        int last = cardPoints.length - 1 ;  // to track from last 
         end--;
+        // Apply silding window to travel each window
         while ( end >= 0 ) {
             localMax += cardPoints[last];
             localMax -= cardPoints[end] ; 
-            max = Math.max ( max , localMax ) ;
+            maxCardValue = Math.max ( maxCardValue , localMax ) ; // if max found update max 
             end--;
             last--;
         }
-        return max ; 
+        return maxCardValue ; // return maximum card value
     }
 }
