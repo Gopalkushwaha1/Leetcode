7dec9c27e181f87b8319b24a66c54940cdb26610
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-17 13:28:05 +0530

    Time: 2 ms (100%), Space: 47.6 MB (48.33%) - LeetHub

---
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int maxCustomerSatisfied = 0  ;
+        int localmaxCustomersSatisfied = 0 ;
+        int max = 0 ; 
+        int start = 0 ; 
+        int end = 0 ; 
+
+        // Calculating satisfied customers 
+        for ( int i = 0 ; i < customers.length ; i++ ) {
+            maxCustomerSatisfied += customers[i] * (1 - grumpy[i]) ; 
+        } 
+        
+        // Making Sliding window 
+        for ( end = 0 ; end < minutes ; end++ ) {
+            localmaxCustomersSatisfied += customers[end] * grumpy[end] ; 
+            max = Math.max( max , localmaxCustomersSatisfied) ;
+        }
+
+        // Travel this window to last ; 
+        while ( end < customers.length ) {
+            localmaxCustomersSatisfied += customers[end] * grumpy[end] ;
+            localmaxCustomersSatisfied -= customers[start] * grumpy[start] ;
+            end ++ ;
+            start++ ; 
+            max = Math.max( max , localmaxCustomersSatisfied) ; 
+        }
+        return maxCustomerSatisfied + max ; 
+    }
+}
