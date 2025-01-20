b240c823b15737cb769af4c1b12d44f780514241
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-20 14:57:42 +0530

    Time: 3 ms (85.77%), Space: 46.5 MB (53.91%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,18 +1,18 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int start = 0 , end = 0 , maxLen = 0 , zeroCount = 0 ;
-        for ( int ele : nums ) if ( ele == 0 ) zeroCount++ ; 
-        if ( zeroCount <=  k ) return nums.length ; 
-        while ( end < nums.length ) {
+        int start = 0 , end = 0 , maxLen = 0 , zeroCount = 0 ;  // define prerequisite varibale 
+        for ( int ele : nums ) if ( ele == 0 ) zeroCount++ ;   // count total zero 
+        if ( zeroCount <=  k ) return nums.length ;            // if k is more than count 0 return len 
+        while ( end < nums.length ) {                   // making sliding window 
             
-            if ( nums[end] == 0 ) k-- ;
+            if ( nums[end] == 0 ) k-- ;          // checking if ele is 0 reduce the window 
             while ( k < 0 ) {
-                if(nums[start] == 0 ) k++ ;
+                if(nums[start] == 0 ) k++ ;          // slink the window
                 start++ ;
             }
-             maxLen = Math.max ( maxLen , end - start  + 1 ) ;
-            end++ ; 
+            maxLen = Math.max ( maxLen , end - start  + 1 ) ;        // calculating total len 
+            end++ ;                                                // increase the window 
         }
-        return maxLen  ; 
+        return maxLen  ; // return ans 
     }
 }
