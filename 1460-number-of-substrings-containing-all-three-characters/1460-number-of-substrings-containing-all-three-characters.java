dffd18f3b7007630ea34bf6de58185908ab039b9
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-20 10:33:48 +0530

    Time: 13 ms (56.1%), Space: 44.6 MB (63.91%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -4,18 +4,18 @@ class Solution {
        int end = 0 ; 
        int n = s.length() - 1 ; 
        int count = 0 ;
-       int[] arr = new int[3]; 
+       int[] arr = new int[3]; // to store count of a , b , c 
 
        while ( end < s.length() ) {
-        int idx = s.charAt(end) ; 
-        arr[idx - 'a']++ ;
-        while ( arr[0] >= 1 && arr[1] >= 1 && arr[2] >= 1 ) {
+        int idx = s.charAt(end) ; // geting idx of char a -> 0 , b -> 1 , c -> 2 
+        arr[idx - 'a']++ ;        // update the occureance of char by 1 
+        while ( arr[0] >= 1 && arr[1] >= 1 && arr[2] >= 1 ) { //if all came (a,b,c)then update the count 
             count += n - end + 1 ;
             arr[s.charAt(start) - 'a']--;
             start++;
         }
         end++;
        }
-       return count ; 
+       return count ;   // return count 
     }
 }
