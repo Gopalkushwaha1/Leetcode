746fe570fee4c9f37b74763036cb608e4c823a38
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-20 08:31:25 +0530

    Time: 1 ms (94.75%), Space: 43.3 MB (36.77%) - LeetHub

--- 3463-alternating-groups-i/3463-alternating-groups-i.java
+++ 3463-alternating-groups-i/3463-alternating-groups-i.java
@@ -1,14 +1,14 @@
 class Solution {
     public int numberOfAlternatingGroups(int[] colors) {
-        int n = colors.length ; 
-        int count = 0 ;
+        int count = 0 ; // count variable for total count 
+        int n = colors.length ; // len of arr
 
+        // checking valid ans 
         for ( int i = 0 ; i < n ; i++ ) {
-            int next = ( i + 1 ) % n ;
-            int nextNext = ( i + 2 ) % n ;
-
-            if ( colors[i] != colors[next] && colors[next] != colors[nextNext]) count++ ; 
+            if ( colors[i] != colors[(i+1)%n] && colors[(i+1)%n] != colors[(i+2)%n]) {
+                count++ ; // if condition valid increase count by 1 ; 
+            }
         }
-        return count ; 
+        return count ; // return count 
     }
 }
