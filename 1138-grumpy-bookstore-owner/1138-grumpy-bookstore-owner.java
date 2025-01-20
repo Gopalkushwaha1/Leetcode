79195561207fd572584c1fcd38db5570f85f399d
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-20 14:01:54 +0530

    Time: 2 ms (100%), Space: 46.3 MB (93.68%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.java
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.java
@@ -1,32 +1,24 @@
 class Solution {
-    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-        int maxCustomerSatisfied = 0  ;
-        int localmaxCustomersSatisfied = 0 ;
-        int max = 0 ; 
-        int start = 0 ; 
-        int end = 0 ; 
-
-        // Calculating satisfied customers 
-        for ( int i = 0 ; i < customers.length ; i++ ) {
-            maxCustomerSatisfied += customers[i] * (1 - grumpy[i]) ; 
-        } 
-        
-        // Making Sliding window 
-        for ( end = 0 ; end < minutes ; end++ ) {
-            localmaxCustomersSatisfied += customers[end] * grumpy[end] ; 
-            max = Math.max( max , localmaxCustomersSatisfied) ;
+    public int maxSatisfied(int[] customer, int[] grumpy, int minutes) {
+        int satisfiedCust = 0 , start = 0 , end = 0 , unsatisfiedCust = 0  , maxUnsatisfiedCust = 0 ; 
+        //count staisfied customer 
+        for ( int i = 0 ; i < grumpy.length ; i++ ) {  // TC :-> O(n)
+            satisfiedCust += customer[i] * (1 - grumpy[i]) ; 
         }
 
-        // Travel this window to last ; 
-        while ( end < customers.length ) {
-            localmaxCustomersSatisfied += customers[end] * grumpy[end] ;
-            localmaxCustomersSatisfied -= customers[start] * grumpy[start] ;
-            end ++ ;
-            start++ ; 
-            max = Math.max( max , localmaxCustomersSatisfied) ; 
+        // Calculating unsatisfied customer 
+        for ( end = 0 ;  end < minutes ; end++ ) {   // TC :-> O(minutes)
+            unsatisfiedCust += customer[end] * grumpy[end] ; 
         }
+        maxUnsatisfiedCust = Math.max( maxUnsatisfiedCust , unsatisfiedCust) ; // update maxUnsatisfiedCust
 
-        // return total satisfied customer 0 & 1 
-        return maxCustomerSatisfied + max ; 
+        while ( end < grumpy.length ) {  // TC -> 0(n)          // sliding window for mintues
+            unsatisfiedCust += customer[end] * grumpy[end] ;       // increase the window by 1 
+            unsatisfiedCust -= customer[start] * grumpy[start] ;   // slink the window by 1 
+            maxUnsatisfiedCust = Math.max( maxUnsatisfiedCust , unsatisfiedCust) ;   // update ans 
+            end++; start++ ;        // update window 
+
+        }
+        return maxUnsatisfiedCust + satisfiedCust ; // return ans 
     }
 }
