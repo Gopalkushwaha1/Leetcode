ae0e9e9104d8ead92985d550b51ece2a0d6c4227
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-20 09:51:54 +0530

    Time: 3 ms (90.73%), Space: 61.1 MB (27.88%) - LeetHub

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,26 +1,23 @@
 class Solution {
-    public int numOfSubarrays(int[] arr, int k, int threshold) {
-        int count = 0 ;  // to store number of count variable 
+    public int numOfSubarrays(int[] arr, int k, int t) {
+        int count = 0 ; 
+        int end = 0 ; 
         int start = 0 ;
-        int end   = 0 ; 
-        int sum   = 0 ;
-        // Making Sliding window of kth size 
+        int sum = 0 ; 
+        // creating window of len k 
         for ( end = 0 ; end < k ; end++ ) {
             sum += arr[end] ; 
-        } 
-        if ( sum >= k * threshold ) {
-                count++ ; 
-            }   
-
-        // Moving Sliding window 
+        }
+        if ( sum >= t * k ) {
+            count++ ; 
+        }
         while ( end < arr.length ) {
             sum += arr[end] ;
             sum -= arr[start] ;
-            if ( sum >= k * threshold ) {
-                count++ ; 
-            }
-            end++ ;  
-            start++ ; 
+
+            if ( sum >= t * k) count++ ;
+            end++;
+            start++;
         }
         return count ; 
     }
