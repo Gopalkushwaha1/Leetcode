1c79bb3ec0cd71e757bd482f9f06b47565360dd9
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-21 10:44:01 +0530

    Time: 11 ms (90.75%), Space: 44.4 MB (87.98%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,21 +1,30 @@
 class Solution {
     public int numberOfSubstrings(String s) {
         int a = 0 , b = 0 , c = 0 ;
-        // int[] arr = new int[3] ;                         // to count freq for valid check
-        int start = 0 , end = 0 , count = 0 ;            // define prerequisite variable
-        while ( end < s.length()) {                      // Apply sliding window move end till end 
+        int start = 0 , end = 0 , count = 0 ;  
+
+        while ( end < s.length()) {  
+
             if(s.charAt(end) == 'a')a++;
+
             else if(s.charAt(end) == 'b')b++;
-            else c++ ;                            // increment the idx 
-            while(a >= 1 && b >= 1 && c >= 1 ) {   // count valid ans and slink window
-                count += (s.length() - end  ) ;          // update count 
+
+            else c++ ;   
+                                     
+            while(a >= 1 && b >= 1 && c >= 1 ) { 
+
+                count += (s.length() - end  ) ;   
+
                 if(s.charAt(start) == 'a')a--;
+
                 else if(s.charAt(start) == 'b')b--;
-                else c--;                  // slink the window
-                start++;                                 // moving start variable 
+
+                else c--;     
+
+                start++;                                  
             }
-            end++ ;                                      // increment the end 
+            end++ ;                                       
         }
-        return count ;                                   // return count 
+        return count ;                                   
     }
 }
