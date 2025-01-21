3cefd8816f62b5f513aac62bf7a62d2777cd9cbf
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-21 10:34:20 +0530

    Time: 12 ms (72.99%), Space: 44.7 MB (51.28%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,14 +1,17 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int[] arr = new int[3] ;                         // to count freq for valid check
+        int a = 0 , b = 0 , c = 0 ;
+        // int[] arr = new int[3] ;                         // to count freq for valid check
         int start = 0 , end = 0 , count = 0 ;            // define prerequisite variable
         while ( end < s.length()) {                      // Apply sliding window move end till end 
-            int idx = s.charAt(end) ;                    // calculating idx of char 
-            arr[idx - 'a']++;                            // increment the idx 
-            while(arr[0] >= 1 && arr[1] >= 1 && arr[2] >= 1 ) {   // count valid ans and slink window
+            if(s.charAt(end) == 'a')a++;
+            else if(s.charAt(end) == 'b')b++;
+            else c++ ;                            // increment the idx 
+            while(a >= 1 && b >= 1 && c >= 1 ) {   // count valid ans and slink window
                 count += (s.length() - end  ) ;          // update count 
-                int startidx = s.charAt(start) ;         // calculating start index 
-                arr[startidx - 'a']--;                   // slink the window
+                if(s.charAt(start) == 'a')a--;
+                else if(s.charAt(start) == 'b')b--;
+                else c--;                  // slink the window
                 start++;                                 // moving start variable 
             }
             end++ ;                                      // increment the end 
