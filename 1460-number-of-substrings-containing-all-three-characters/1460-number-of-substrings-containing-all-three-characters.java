a9103ea44885831433e31ce7ed766213d293c766
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-21 10:21:52 +0530

    Time: 16 ms (31.51%), Space: 44.8 MB (39.82%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,18 +1,18 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int[] arr = new int[3] ; 
-        int start = 0 , end = 0 , count = 0 ; 
-        while ( end < s.length()) {
-            int idx = s.charAt(end) ;
-            arr[idx - 'a']++;
-            while(arr[0] >= 1 && arr[1] >= 1 && arr[2] >= 1 ) {
-                count += (s.length() - end  ) ; 
-                int startidx = s.charAt(start) ;
-                arr[startidx - 'a']--;
-                start++;
+        int[] arr = new int[3] ;                         // to count freq for valid check
+        int start = 0 , end = 0 , count = 0 ;            // define prerequisite variable
+        while ( end < s.length()) {                      // Apply sliding window move end till end 
+            int idx = s.charAt(end) ;                    // calculating idx of char 
+            arr[idx - 'a']++;                            // increment the idx 
+            while(arr[0] >= 1 && arr[1] >= 1 && arr[2] >= 1 ) {   // count valid ans and slink window
+                count += (s.length() - end  ) ;          // update count 
+                int startidx = s.charAt(start) ;         // calculating start index 
+                arr[startidx - 'a']--;                   // slink the window
+                start++;                                 // moving start variable 
             }
-            end++ ; 
+            end++ ;                                      // increment the end 
         }
-        return count ; 
+        return count ;                                   // return count 
     }
 }
