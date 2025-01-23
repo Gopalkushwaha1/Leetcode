f778486d34f6883112a67a63e9c082640bda0d04
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-23 08:34:29 +0530

    Time: 8 ms (99.45%), Space: 64.7 MB (8.01%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,33 +1,29 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int[] leftSum = new int[nums.length] ; 
-        int[] rightSum = new int[nums.length] ; 
-        int[] result = new int[nums.length - k + 1 ] ; 
+        int n = nums.length ; 
+        int[] leftravelMax = new int[n] ;
+        int[] rightravelMax = new int[n] ;
+        int[] result = new int[ n - k + 1 ] ;
 
-        // filling leftSum 
-        for ( int i = 0 ; i < nums.length ; i++ ) {
-            if(i % k  == 0 ) {
-                leftSum[i] = nums[i] ; 
-            }
-            else {
-                leftSum[i] = Math.max(leftSum[i-1] , nums[i]);
-            }
-        }
-       // filling RightSum
-       for ( int i = nums.length - 1 ; i >= 0 ; i-- ) {
-        if ( i == nums.length -1 || (i + 1 ) % k == 0 ) {
-            rightSum[i] = nums[i] ; 
-        }
-        else {
-            rightSum[i] = Math.max(nums[i] , rightSum[i+1]);
-        }
-       }
+        for ( int i = 0 ; i < n ; i++ ) {
+            
+            if ( i % k == 0 ) leftravelMax[i] = nums[i] ;
 
-       // filling Result 
-       for ( int i = 0 ; i < result.length ; i++ ) {
-        result[i] = Math.max(rightSum[i] , leftSum[i+k - 1]);
-       } 
+            else leftravelMax[i] = Math.max(leftravelMax[i - 1] , nums[i]) ; 
 
-       return result ; // return ans 
+        }
+        for ( int i = n -1  ; i >= 0  ; i-- ) {
+            
+            if ( (i + 1 ) % k == 0 || i == n - 1 ) rightravelMax[i] = nums[i] ;
+
+            else rightravelMax[i] = Math.max(rightravelMax[i + 1] , nums[i]) ; 
+
+        }
+        for ( int i = 0 ; i < result.length ; i++ ) {
+            
+            result[i] = Math.max(rightravelMax[i] , leftravelMax[i+k-1] ) ;
+
+        }
+        return result ; 
     }
 }
