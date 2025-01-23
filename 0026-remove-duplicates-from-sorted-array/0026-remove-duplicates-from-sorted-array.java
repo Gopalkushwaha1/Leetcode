5fda9b4fcfa3fb306fabfa7609be7a43b8f4ed2f
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-23 11:06:51 +0530

    Time: 1 ms (65.16%), Space: 45.1 MB (12.2%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -4,14 +4,14 @@ class Solution {
 
         for ( int k = 0 ; k < nums.length ; k++ ) {
 
-            if( k + 1 == nums.length  ) {
-                nums[i++] = nums[k] ; 
-            }
+            if( k + 1 == nums.length  )  nums[i++] = nums[k] ;
+            
             else if(nums[k] != nums[k+1] ) {
                 nums[i] = nums[k] ; 
                 i++;
             }
         }
+        
         return i ; 
     }
 }
