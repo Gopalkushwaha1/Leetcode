907de7d2266736a9a550652a513310134b492539
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-25 08:55:33 +0530

    Time: 8 ms (5.36%), Space: 41.7 MB (47.86%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -6,20 +6,14 @@ class Solution {
 
         int num1 = 0 , nums2 = 0 ; 
         System.out.println("num1 " + num1) ;
-        while ( i < v1.length() && v1.charAt(i) != '.') {
-            int a = v1.charAt(i) - '0' ; 
-            System.out.println("n " + i + " " + a );
-            
-            num1 = num1 * 10 + a ; 
-            System.out.println("num1 " + num1) ;
+        while ( i < v1.length() && v1.charAt(i) != '.') { 
+            num1 = num1 * 10 + (v1.charAt(i) - '0' ) ; 
             i++ ; 
         }
-        System.out.println("num1 " + num1) ;
         while ( j < v2.length() && v2.charAt(j) != '.') {
             nums2 = nums2 * 10 + (v2.charAt(j) - '0' ) ; 
             j++ ; 
         }
-        System.out.println("num2" + nums2) ;
         if ( num1 > nums2 ) return 1 ;
         else if ( nums2 > num1) return -1 ; 
         i++ ;
