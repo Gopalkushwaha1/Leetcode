79cade4feb5854d37a105c75760e8db0ef354cc8
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-25 09:01:07 +0530

    Time: 0 ms (100%), Space: 41.1 MB (97.81%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,24 +1,30 @@
 class Solution {
     public int compareVersion(String v1, String v2) {
+
+       // Define variable 
        int i  = 0 , j = 0 ; 
-
+       // checking all version separate by '.'   
        while ( i < v1.length() || j < v2.length() ) {
-
+        // variable of comapre version v1 and v2 
         int num1 = 0 , nums2 = 0 ; 
-
+        // calculating version v1 
         while ( i < v1.length() && v1.charAt(i) != '.') { 
             num1 = num1 * 10 + (v1.charAt(i) - '0' ) ; 
             i++ ; 
         }
+        // calculating version v2 
         while ( j < v2.length() && v2.charAt(j) != '.') {
             nums2 = nums2 * 10 + (v2.charAt(j) - '0' ) ; 
             j++ ; 
         }
+        // checking condition 
         if ( num1 > nums2 ) return 1 ;
         else if ( nums2 > num1) return -1 ; 
-        i++ ;
-        j++ ; 
+        // update i & j if not satisfy condition 
+        i++ ; j++ ;
+         
        }
-       return 0 ; 
+
+       return 0 ; // return 0 if version not greater 
     }
 }
