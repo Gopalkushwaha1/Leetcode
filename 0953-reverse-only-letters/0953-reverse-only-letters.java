32e607d3059c43a4b7fa222b165f4d3e5c4b5753
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-25 22:26:40 +0530

    Time: 0 ms (100%), Space: 41.8 MB (42.28%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.java
+++ 0953-reverse-only-letters/0953-reverse-only-letters.java
@@ -2,13 +2,12 @@ class Solution {
     public void reverse(char[] ch , int start , int end) {
 
         while ( start < end ) {
-            while (start < ch.length &&  (ch[start] < 65 || (ch[start] > 90  && ch[start] < 97 )) ) {
-                start++ ; 
-            }
-            while ( end >= 0 &&  (ch[end] < 65 || (ch[end] > 90 && ch[end] < 97 )) ) {
-                end-- ; 
-            }
+
+            while (start < ch.length && (ch[start] < 65||(ch[start] > 90 && ch[start] < 97 ))) start++ ;
+            while ( end >= 0 &&  (ch[end] < 65 || (ch[end] > 90 && ch[end] < 97 )) ) end-- ;
+
             if(start >= end ) break ; 
+            
             char c = ch[start] ; 
             ch[start] = ch[end] ;
             ch[end] = c ; 
