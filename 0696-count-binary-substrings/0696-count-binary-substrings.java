d14d93d11f33ade0e65281c57852b3e4ef8b1490
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-27 08:38:07 +0530

    Time: 10 ms (84.33%), Space: 45.4 MB (27.98%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.java
+++ 0696-count-binary-substrings/0696-count-binary-substrings.java
@@ -1,21 +1,20 @@
 class Solution {
     public int countBinarySubstrings(String s) {
-        int prevCount = 0;
-        int count = 0;
-        char prevChar = s.charAt(0);
-        int sum = 0;
+        // Define prerequisite variable
+        int count = 0 , currGroup = 1 , prevGroup = 0 ; 
 
-        for (char c : s.toCharArray()) {
-            if (c == prevChar) {
-                count++;
-            } else {
-                prevChar = c;
-                sum += Math.min(count, prevCount);
-                prevCount = count;
-                count = 1;
+        // find diff group ( continues 0 or 1 )    
+        for ( int i = 1 ; i < s.length() ; i++ ) {
+            // if same value update curr Group 
+            if ( s.charAt(i) == s.charAt(i-1) ) currGroup++ ; 
+            // update count 
+            else {
+                count += Math.min ( prevGroup , currGroup ) ;
+                prevGroup = currGroup ; 
+                currGroup = 1 ; 
             }
-        }
+        } 
 
-        return sum + Math.min(count, prevCount);
+        return count + Math.min( prevGroup , currGroup ) ; 
     }
 }
