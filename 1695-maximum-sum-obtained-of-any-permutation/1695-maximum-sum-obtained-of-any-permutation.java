2835597408298047aa0752bf55d88880215542ef
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-28 15:26:19 +0530

    Time: 25 ms (75.14%), Space: 79.5 MB (42.94%) - LeetHub

--- 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.java
+++ 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.java
@@ -18,7 +18,7 @@ class Solution {
         long sum = 0 ; 
 
         for ( int i = prefixSum.length - 1 ; i > 0 ; i-- ) {
-            sum  = (sum + ((long)nums[i-1] * (long)prefixSum[i] ) % 1000000007 )% 1000000007; 
+            sum  +=  (long)nums[i-1] * prefixSum[i]  ; 
         }
 
         return (int)(sum % 1000000007 ) ; 
