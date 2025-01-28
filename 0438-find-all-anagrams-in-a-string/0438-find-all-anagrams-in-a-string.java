35581d6ad2ee3faccfc5c871302b32eaa3df295d
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-28 16:14:26 +0530

    Time: 10 ms (81.49%), Space: 44.8 MB (71.24%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,20 +1,15 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        
-        int start = 0 , end = Math.min(s.length() , p.length()); 
+        List<Integer> result = new ArrayList<>() ;
+        if ( p.length() > s.length() ) return result;
+        int start = 0 ; 
         int[] ans = new int[26] ; 
         int[] ans2 = new int[26] ; 
 
-        for ( int i = 0 ; i < end ; i++ ) {
+        for ( int i = 0 ; i < p.length() ; i++ ) {
             ans[p.charAt(i) - 'a']++ ; 
             ans2[s.charAt(i) - 'a']++ ;
-        }
-        for ( int i = 0 ; i < ans.length ; i++ ) {
-            System.out.println(ans[i] + " " + ans2[i] ) ; 
-        }
-        List<Integer> result = new ArrayList<>() ;
-        if ( p.length() > s.length() ) return result; 
-        System.out.println(Arrays.equals(ans,ans2) ) ;
+        } 
         if(Arrays.equals(ans,ans2)) result.add(0); 
         for( int i = p.length() ; i < s.length() ; i++ ) {
             ans2[s.charAt(i) - 'a']++;
