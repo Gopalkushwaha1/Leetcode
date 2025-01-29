697ba9f8531ff43ccb47735a46c4ebba0c5c62ae
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-29 08:27:35 +0530

    Time: 8 ms (99.21%), Space: 64.7 MB (9.11%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,28 +1,32 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        if ( k == 1 ) return nums ; 
         int n = nums.length ; 
-        int[] leftravelMax = new int[n] ;
-        int[] rightravelMax = new int[n] ;
-        int[] result = new int[ n - k + 1 ] ;
+        int[] leftSum = new int[n] ;
+        int[] rightSum = new int[n] ; 
+        int[] result = new int[n-k+1] ;
 
+        // Filling left sum 
         for ( int i = 0 ; i < n ; i++ ) {
-            
-            if ( i % k == 0 ) leftravelMax[i] = nums[i] ;
-
-            else leftravelMax[i] = Math.max(leftravelMax[i - 1] , nums[i]) ; 
-
+            if( i % k == 0 ) {
+                leftSum[i] = nums[i] ; 
+            } 
+            else {
+                leftSum[i] = Math.max ( leftSum[i-1] , nums[i] ) ;
+            }
         }
-        for ( int i = n -1  ; i >= 0  ; i-- ) {
-            
-            if ( (i + 1 ) % k == 0 || i == n - 1 ) rightravelMax[i] = nums[i] ;
-
-            else rightravelMax[i] = Math.max(rightravelMax[i + 1] , nums[i]) ; 
-
+        // Filling right sum 
+        for ( int i = n-1 ; i >= 0  ; i-- ) {
+            if( i == n-1 ||  (i+1) % k == 0 ) {
+                rightSum[i] = nums[i] ; 
+            } 
+            else {
+                rightSum[i] = Math.max ( rightSum[i+1] , nums[i] ) ;
+            }
         }
+        // Filling result 
         for ( int i = 0 ; i < result.length ; i++ ) {
-            
-            result[i] = Math.max(rightravelMax[i] , leftravelMax[i+k-1] ) ;
-
+            result[i] = Math.max( leftSum[i+k-1] , rightSum[i]);
         }
         return result ; 
     }
