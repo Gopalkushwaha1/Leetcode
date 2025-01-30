215a32c4ab7493b26fcd79e2ceda1ad45ebac044
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-30 13:00:08 +0530

    Time: 8 ms (58.02%), Space: 59.8 MB (18.95%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.java
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,17 +1,23 @@
 class Solution {
     public int minSwaps(int[] nums) {
-        int countOne = 0 , countZero = 0 , minSwap ; 
-        
-        for ( int ele : nums ) if ( ele == 1 ) countOne++ ; // counting total occurence of 1 
-        for ( int i = 0 ; i < countOne ; i++ ) if ( nums[i] == 0 ) countZero++ ; // count 0 in window
-
-        minSwap = countZero ; 
-        // Sliding window to count minSwap
-        for ( int start = 0 , end = countOne ; start < nums.length ; start++ , end++ ) {
-            if ( nums[(end) % nums.length] == 0 ) countZero++ ;  // increase window
-            if ( nums[start] == 0 ) countZero--;                 // slink window
-            minSwap = Math.min(minSwap , countZero ) ;           // calculate min swap
+        int oneCount = 0  , start = 0 , i = 0  ;
+        for ( int ele : nums ) {
+            if ( ele == 1 ) oneCount++ ; 
         }
-        return minSwap ; // return min swap
+
+        int zeroCount = 0 , minSwap = 0 ; 
+
+        for ( start = 0 ; start < oneCount ; start++ ) {
+            if(nums[start] == 0 ) zeroCount++ ; 
+        }
+        minSwap = zeroCount ; 
+
+        while ( start < nums.length + oneCount - 1 ) {
+            if ( nums[start % nums.length] == 0 ) zeroCount++;
+            if ( nums[i] == 0 ) zeroCount--;
+            minSwap = Math.min ( minSwap , zeroCount) ; 
+            start++ ; i++ ; 
+        } 
+        return minSwap ; 
     }
 }
