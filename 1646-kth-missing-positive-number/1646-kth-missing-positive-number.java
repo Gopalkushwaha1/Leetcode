a76eaf493343286d7fd11fdb4c7f034f3502ae92
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-01-30 14:24:06 +0530

    Time: 0 ms (100%), Space: 42.8 MB (56.83%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -1,26 +1,26 @@
-// class Solution {
-//     public int findKthPositive(int[] arr, int k) {
-//         int count = 1 , idx = 0 ; 
-
-//         while  ( k > 0 && idx < arr.length ) {
-//             if ( count == arr[idx] ) count++ ;
-//             else k-- ;
-//             idx++ ; 
-//         }
-
-//         return k != 0 ? arr[arr.length - 1 ] + k : idx - 1 ; 
-//     }
-// }
-
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int i = 1 ;
-        int idx = 0 ;
-        while ( k > 0 && idx < arr.length ) {
-            if (i == arr[idx]) idx ++ ;
-            else k-- ; 
-            i++ ; 
+        int count = 1 , idx = 0 ; 
+
+        while  ( k > 0 && idx < arr.length ) {
+            if ( count == arr[idx] ) idx++ ;
+            else k-- ;
+            count++ ; 
         }
-        return k != 0 ? arr[arr.length-1]+k : i-1 ; 
+
+        return k != 0 ? arr[arr.length - 1 ] + k : count - 1 ; 
     }
 }
+
+// class Solution {
+//     public int findKthPositive(int[] arr, int k) {
+//         int i = 1 ;
+//         int idx = 0 ;
+//         while ( k > 0 && idx < arr.length ) {
+//             if (i == arr[idx]) idx ++ ;
+//             else k-- ; 
+//             i++ ; 
+//         }
+//         return k != 0 ? arr[arr.length-1]+k : i-1 ; 
+//     }
+// }
