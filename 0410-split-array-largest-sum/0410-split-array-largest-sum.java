0c93c635b51bba646171e3f549adc659ba63f79a
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-01 09:46:34 +0530

    Time: 1 ms (57.68%), Space: 41.4 MB (53.77%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,55 @@
+class Solution {
+    // Function to check this mid value possible or not 
+    public boolean isValid( int[] nums , int k , int mid ) {
+        int count = 1 , idx = 0 , sum = 0 ; 
+
+        // Travel each ele in array and check 
+        while ( idx < nums.length ) {
+            if ( count > k ) return false ; 
+            int num = nums[idx] ;
+            // If ele is greater then mid not possbile 
+            if ( num > mid ) return false ;  
+            // Check if possible 
+            else if ( num + sum <= mid ) {
+                sum += num ; 
+                idx++ ; 
+            }
+            // If sum greater than mid split update 
+            else {
+                count++;
+                sum = num ; 
+                idx++ ; 
+            }   
+        }
+        return count <= k ;
+    }
+    public int splitArray(int[] nums, int k) {
+        int min = 0 ; 
+        int max = 0 ;
+
+        // Calculating min max 
+        for ( int ele : nums ) {
+            min = Math.max ( ele , min ) ;
+            max += ele ; 
+        } 
+        if ( k == 1 ) return max ;  
+        // Max will be total sum minus min 
+        max -= min ; 
+
+        while ( min <= max ) {
+            // Finding mid value check if possible 
+            int mid = (min + max ) / 2 ; 
+            // If possible then update in max 
+            if ( isValid( nums , k , mid  )){
+                max = mid - 1 ;
+            }
+            // If not then update min
+            else {
+                min = mid + 1 ; 
+            } 
+        
+        }
+        // Return min 
+        return min ; 
+    }
+}
