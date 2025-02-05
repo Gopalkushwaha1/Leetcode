b25829d3ef1f2490b91e22e3037c791848141fee
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-05 09:26:23 +0530

    Time: 0 ms (100%), Space: 41.3 MB (94.96%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -4,14 +4,15 @@ class Solution {
 
         for ( int i = 0 ; i < s1.length() ; i++ ) {
             if ( s1.charAt(i) != s2.charAt(i)) {
-                if ( first == -1 ) first = i ; 
-                else if (second == -1) second = i ;
-                else return false ;  
+                if ( first == -1 ) first = i ;   // We got our first unequal char 
+                else if (second == -1) second = i ;   // get 2nd unequal char 
+                else return false ;  // More than two char diff 
             }
         }
-        if ( first == -1 ) return true ; 
-        if ( second == - 1 ) return false ; 
+        if ( first == -1 ) return true ; // It means it already sorted 
+        if ( second == - 1 ) return false ;  // It means only 1 char is different 
 
+        // Check for Alternate char 
         return s1.charAt(first) == s2.charAt(second) && s1.charAt(second) == s2.charAt(first) ; 
     }
 }
