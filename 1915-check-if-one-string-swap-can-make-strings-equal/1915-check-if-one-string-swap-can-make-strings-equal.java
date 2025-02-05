e95692b407412fb0e3f053d5d83b1fd3e3a3ac45
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-05 09:12:33 +0530

    Time: 0 ms (100%), Space: 41.5 MB (76.92%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,30 +1,30 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-       // Check At which indices char is Diff
-       int idx = 0 ; 
-       for ( idx = 0 ; idx < s1.length() ; idx++ ) {
-        if ( s1.charAt(idx) != s2.charAt(idx)) break ; 
-       } 
-       // Indices reach at Last index it means it is sorted no swap need 
-       if ( idx == s1.length()) return true ; 
-       char ch = s2.charAt(idx) ; 
-       int swapidx = 0 ; 
-       for ( swapidx = idx + 1 ; swapidx < s1.length() ; swapidx++ ) {
-        if (s1.charAt(swapidx) == ch && s1.charAt(idx) == s2.charAt(swapidx) ) break ; 
-       }
-       if ( swapidx == s1.length()) return false ; 
-       char[] c = s1.toCharArray() ; 
-       char temp = c[idx] ;
-       c[idx] = c[swapidx] ; 
-       c[swapidx] = temp ; 
-
-       String s = new String(c) ;  
-       for ( int i = 0 ; i < s1.length() ; i++ ) {
-
-        if( s.charAt(i) != s2.charAt(i)) {
-            return false ; 
+        int idx = 0 ; 
+        
+        for  (idx = 0 ; idx < s1.length() ; idx++ ) {
+            if ( s1.charAt(idx) != s2.charAt(idx)) {
+                break ; 
+            }
         }
-       } 
-       return true ; 
+        if ( idx == s1.length()) return true ; 
+        char c1 = s1.charAt(idx) ;
+        char c2 = s2.charAt(idx) ; 
+        int i = 0 ; 
+        for ( i = idx + 1 ; i < s1.length() ; i++ ) {
+            if ( s1.charAt(i) != s2.charAt(i)) {
+                break ; 
+            }
+        }
+        if ( i == s1.length()) return false ; 
+        char c11 = s1.charAt(i) ; 
+        char c22 = s2.charAt(i) ; 
+
+        if ( c1 != c22 || c2 != c11 ) return false ; 
+
+        for ( int j = i+1 ; j < s1.length() ; j++ ) {
+            if ( s1.charAt(j) != s2.charAt(j)) return false ; 
+        }
+        return true ; 
     }
 }
