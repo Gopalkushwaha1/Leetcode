1c1fd2400d19453d032b27e74295da358170f934
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-05 09:24:22 +0530

    Time: 0 ms (100%), Space: 41.3 MB (94.96%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,30 +1,17 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-        int idx = 0 ; 
-        
-        for  (idx = 0 ; idx < s1.length() ; idx++ ) {
-            if ( s1.charAt(idx) != s2.charAt(idx)) {
-                break ; 
-            }
-        }
-        if ( idx == s1.length()) return true ; 
-        char c1 = s1.charAt(idx) ;
-        char c2 = s2.charAt(idx) ; 
-        int i = 0 ; 
-        for ( i = idx + 1 ; i < s1.length() ; i++ ) {
+        int first = -1 , second = -1 ; 
+
+        for ( int i = 0 ; i < s1.length() ; i++ ) {
             if ( s1.charAt(i) != s2.charAt(i)) {
-                break ; 
+                if ( first == -1 ) first = i ; 
+                else if (second == -1) second = i ;
+                else return false ;  
             }
         }
-        if ( i == s1.length()) return false ; 
-        char c11 = s1.charAt(i) ; 
-        char c22 = s2.charAt(i) ; 
+        if ( first == -1 ) return true ; 
+        if ( second == - 1 ) return false ; 
 
-        if ( c1 != c22 || c2 != c11 ) return false ; 
-
-        for ( int j = i+1 ; j < s1.length() ; j++ ) {
-            if ( s1.charAt(j) != s2.charAt(j)) return false ; 
-        }
-        return true ; 
+        return s1.charAt(first) == s2.charAt(second) && s1.charAt(second) == s2.charAt(first) ; 
     }
 }
