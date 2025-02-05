0cca0dc038631778ee64620cbfa40ce8fa5f43a4
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-05 09:00:33 +0530

    Time: 1 ms (52.21%), Space: 41.5 MB (89.79%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -18,12 +18,10 @@ class Solution {
        c[idx] = c[swapidx] ; 
        c[swapidx] = temp ; 
 
-       String s = new String(c) ;
-       System.out.println("New String " + s ) ;  
+       String s = new String(c) ;  
        for ( int i = 0 ; i < s1.length() ; i++ ) {
 
         if( s.charAt(i) != s2.charAt(i)) {
-            System.out.print("Not equal At : " + i ) ; 
             return false ; 
         }
        } 
