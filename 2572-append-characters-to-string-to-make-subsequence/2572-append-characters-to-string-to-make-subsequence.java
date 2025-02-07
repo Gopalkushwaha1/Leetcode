570f503e33691fb3a7ffa6ee6f97644275d5363d
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-07 11:33:20 +0530

    Time: 5 ms (84.99%), Space: 45.7 MB (25.91%) - LeetHub

--- 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.java
+++ 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.java
@@ -1,15 +1,16 @@
 class Solution {
     public int appendCharacters(String s, String t) {
-         int slen = s.length() ; 
-         int tlen = t.length() ; 
-         int i = 0 , j = 0 ; 
-
+         int slen = s.length() ;   // length of s
+         int tlen = t.length() ;   // length of t
+         int i = 0 , j = 0 ;       // Variable to track how both string simliar 
+        // Checking similarity 
          while ( j < tlen && i < slen ) {
              if ( s.charAt(i) == t.charAt(j)){
                 j++; 
              }
              i++ ; 
          } 
+         // return total length - unsimilarity 
          return tlen - j ; 
     }
 }
