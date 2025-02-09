821afc6fc1499ae6d3e073d13e393abef8c4901e
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-09 11:42:38 +0530

    Time: 20 ms (12.88%), Space: 45.1 MB (12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,35 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length() ) return false ; 
-        int[] count1 = new int[26];
-        int[] count2 = new int[26];
+        if ( s.length() != t.length()) return false ; 
+        HashMap<Character , Integer > map = new HashMap<>() ;
+        HashMap<Character , Integer > map2 = new HashMap<>() ;  
 
         for ( int i = 0 ; i < s.length() ; i++ ) {
-            count1[s.charAt(i) - 'a']++;
-            count2[t.charAt(i) - 'a']++;
+            if ( map.containsKey(s.charAt(i))){
+                int prev = map.get(s.charAt(i)) ; 
+                map.put(s.charAt(i) , prev + 1 ) ; 
+            }
+            else {
+                map.put(s.charAt(i) , 1 ) ; 
+            }
         }
-        for ( int i = 0 ; i < count1.length ; i++ ) { 
-            if ( count1[i] != count2[i]) { 
-                return false ;
-            } 
+        for ( int i = 0 ; i < t.length() ; i++ ) {
+            if ( map2.containsKey(t.charAt(i))) {
+                int prev = map2.get(t.charAt(i)) ; 
+                map2.put ( t.charAt(i) , prev + 1 ) ; 
+            }
+            else {
+                map2.put ( t.charAt(i) , 1 ) ; 
+            }
+        }
+
+        for ( int i = 0 ; i < s.length() ; i++ ) {
+            char c = s.charAt(i) ; 
+            int freq = map.get(c) ; 
+            if (!map2.containsKey(c)) return false ; 
+            if ( freq != map2.get(c)) {
+                return false ; 
+            }
         }
         return true ; 
     }
