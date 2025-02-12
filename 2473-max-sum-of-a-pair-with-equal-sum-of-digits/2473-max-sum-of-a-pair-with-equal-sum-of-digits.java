42c724e5ec3862bf42ee91d072a74a224b3a8f05
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-12 22:52:08 +0530

    Time: 22 ms (90.86%), Space: 55.1 MB (97.72%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -2,39 +2,24 @@ class Solution {
     public int sumDigit(int ele ) {
         int sum = 0 ; 
         while ( ele != 0 ) {
-            sum += ele%10 ; 
+            sum += ele % 10 ; 
             ele /= 10 ; 
         }
         return sum ; 
     }
     public int maximumSum(int[] nums) {
-        HashMap<Integer , PriorityQueue<Integer>> map = new HashMap<>() ; 
+        int[] arr = new int[82] ; 
+        int ans = -1 ; 
 
-        // Travel each number in nums 
         for ( int ele : nums ) {
-            // Calculate sum of that ele 
-            int sum = sumDigit(ele) ; 
-            // Put in map 
-            if ( map.containsKey(sum)) {
-                map.get(sum).add(ele) ; 
+            int idx = sumDigit(ele) ; 
+
+            if ( arr[idx] != 0 ) {
+                ans = Math.max ( ans , ele + arr[idx]) ; 
             }
-            else {
-                map.put(sum , new PriorityQueue(Collections.reverseOrder()));
-                map.get(sum).add(ele) ; 
-            }
+            arr[idx] = Math.max ( ele , arr[idx] ) ; 
         }
 
-        int max = - 1 ; 
-        // Get max value 
-        for (Map.Entry<Integer , PriorityQueue<Integer>> entry : map.entrySet() ){
-             PriorityQueue<Integer> pq = new PriorityQueue<>(entry.getValue()); // Copy to avoid modifying original
-
-            if (pq.size() > 1) {
-                int first = pq.poll();  // Get the largest element
-                int second = pq.poll(); // Get the second largest element
-                max = Math.max( max , first + second ) ; 
-            }
-        }
-        return max ; 
+        return ans ; 
     }
 }
