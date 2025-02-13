f135d4b38480bca3a5333fbc33ce3fab5b01e51b
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-13 09:20:36 +0530

    Time: 126 ms (96.3%), Space: 69.6 MB (93%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
@@ -4,16 +4,19 @@ class Solution {
         int operation = 0 ; 
         // Add ele in queue 
         for ( int ele : nums ) {
-            long a = ele ; 
-            pq.add(a) ; 
+            if ( ele < k ) {
+                long a = ele ; 
+                pq.add(a) ;
+            } 
         } 
         // modify nums to all ele grater than or equal to k 
-        while (pq.size() > 1 && pq.peek() < k   ) {
+        while (!pq.isEmpty()) {
             long ele1 = pq.poll() ; 
             operation++ ; 
+            if ( pq.isEmpty()) break ; 
             long ele2 = pq.poll() ; 
             long sum = Math.min(ele1 , ele2 ) * 2 + Math.max(ele1 , ele2) ; 
-            pq.add(sum) ;  
+            if ( sum < k ) pq.add(sum) ;  
         }
         return operation ; 
     }
