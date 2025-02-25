ab1bcee30784e09ee5f0fb11d3c421f8f62b9977
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-25 09:29:36 +0530

    Time: 2 ms (84.58%), Space: 46 MB (46.39%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,24 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int i = 0 , j = 0 ; 
 
-        int i = 0 ; // to travel on each ele
-
-        for ( int ele : nums ) {
-
-            if(ele != 0 )  nums[i++] = ele ;  // shift all non zero forward
-
+        while (i < nums.length &&  nums[i] != 0 ) {
+            i++ ;
         }
 
-        while ( i < nums.length ) nums[i++] = 0 ;  // fill all right side zero 
-        
+        j = i+ 1 ; 
+
+        while ( j < nums.length ) {
+            if ( nums[j] != 0 ) {
+                int temp = nums[j] ; 
+                nums[j] = 0 ; 
+                nums[i] = temp ; 
+                i++;
+                j++;
+            }
+            else {
+                j++;
+            }
+        }
     }
 }
