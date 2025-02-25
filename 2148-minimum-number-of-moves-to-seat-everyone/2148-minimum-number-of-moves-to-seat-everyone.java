670d0b53799cd26dc8beca3e0a69b97af0eb13af
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-25 15:03:24 +0530

    Time: 176 ms (2.53%), Space: 45 MB (7.24%) - LeetHub

--- 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.java
@@ -1,16 +1,29 @@
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
+       //  Create n size Array to store position in sorted way 
+       int[] s1 = new int[101] ;
+       int[] s2 = new int[101] ; 
 
-        // Sort the both array to find minium moves 
-        Arrays.sort(seats);
-        Arrays.sort(students);
-        int ans = 0 ; 
+       // Mark the Actual position
+       for ( int i = 0 ; i < seats.length ; i++ ) {
+            s1[seats[i]]++;
+            s2[students[i]]++;
+       }
+       // Make two varibale to track postion 
+       int i = 0 , j = 0 , ans = 0 ; 
+       // Calculate ans 
+       while ( i < 101 && j < 101 ) {
+            while ( i < 101 && s1[i] == 0 ) i++ ; 
+            while ( j < 101 && s2[j] == 0 ) j++ ;
+            System.out.println(Math.abs(i-j) + " " + i + " " + j ) ;  
+            
 
-        // Calculate moves 
-        for ( int i = 0 ; i < seats.length ; i++ ) {
-            ans += Math.abs(seats[i] - students[i]) ; 
-        }
-
-        return ans ; 
+            ans += Math.abs(i-j);
+            if ( i == 101 || j == 101) return ans ; 
+            s1[i]--;
+            s2[j]--;
+            i--; j--; 
+       }
+       return ans ; 
     }
 }
