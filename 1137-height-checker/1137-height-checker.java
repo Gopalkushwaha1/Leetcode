d4fc75aa5b72008761f5b84beea8c6cf86dcd933
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-26 10:47:55 +0530

    Time: 2 ms (89.07%), Space: 41.7 MB (35.41%) - LeetHub

--- 1137-height-checker/1137-height-checker.java
+++ 1137-height-checker/1137-height-checker.java
@@ -1,11 +1,7 @@
 class Solution {
     public int heightChecker(int[] heights) {
         int ans = 0 ; // To store ans 
-        int[] copyHeight = new int[heights.length] ; 
-        for ( int ele : heights ) {
-            copyHeight[ans++] = ele ; 
-        }
-        ans = 0 ; 
+        int[] copyHeight = Arrays.copyOf(heights , heights.length); 
         Arrays.sort(copyHeight) ; 
 
         // count mismatch palce 
