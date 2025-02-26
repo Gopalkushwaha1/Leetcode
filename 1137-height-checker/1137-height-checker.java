083ffe75eba8031244593c0dcbe89ce662ce1102
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-26 11:05:35 +0530

    Time: 0 ms (100%), Space: 41.3 MB (77.05%) - LeetHub

--- 1137-height-checker/1137-height-checker.java
+++ 1137-height-checker/1137-height-checker.java
@@ -1,17 +1,18 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] freq = new int[101];
-        for (int n : heights) {
-            freq[n]++;
+        int[] freq = new int[101] ; 
+        // Fill the freq array
+        for ( int ele : heights ) {
+            freq[ele]++;
         }
 
-        int unexpected = 0;
-        int hi = 0;
-        for (int i = 0; i < freq.length; i++) {
-            for (int j = 0; j < freq[i]; j++) {
-                if (heights[hi++] != i) unexpected++;
+        int hi = 0 , count = 0 ; 
+
+        for ( int i = 0 ; i < 101 ; i++ ) {
+            for ( int j = 0 ; j < freq[i] ; j++ ) {
+                if ( heights[hi++] != i) count++;
             }
         }
-        return unexpected;
+        return count ; 
     }
 }
