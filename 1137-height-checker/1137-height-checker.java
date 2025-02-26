1177242e6ed1f91fb7ba597c9856e18ef16d68f8
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-26 10:54:41 +0530

    Time: 0 ms (100%), Space: 41.7 MB (25.07%) - LeetHub

--- 1137-height-checker/1137-height-checker.java
+++ 1137-height-checker/1137-height-checker.java
@@ -7,7 +7,7 @@ class Solution {
 
         int unexpected = 0;
         int hi = 0;
-        for (int i = 1; i < freq.length; i++) {
+        for (int i = 0; i < freq.length; i++) {
             for (int j = 0; j < freq[i]; j++) {
                 if (heights[hi++] != i) unexpected++;
             }
