bd2d02390bfab87f793187774c76265373216456
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-26 11:16:31 +0530

    Time: 5 ms (99.29%), Space: 46.4 MB (86.95%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,10 +1,26 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        int  ans = 0 ; 
-        Arrays.sort(nums) ; 
-        for ( int i = 0 ; i < nums.length ; i+=2) {
-            ans += nums[i] ; 
+        int[] count = new int[20001]; // For values in range [-10^4, 10^4]
+        
+        // Offset by 10,000 to handle negative values
+        for (int num : nums) {
+            count[num + 10000]++;
         }
-        return ans ; 
+
+        int ans = 0;
+        boolean add = true;
+
+        // Traverse the frequency array
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                if (add) {
+                    ans += i - 10000; // Shift back the offset
+                }
+                add = !add; // Toggle add for pairs
+                count[i]--;
+            }
+        }
+        
+        return ans;
     }
 }
