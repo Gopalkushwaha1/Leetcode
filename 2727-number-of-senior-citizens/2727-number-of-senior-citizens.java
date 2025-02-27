6f0211882b42100a6be9ad828dc2ff556805c60b
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-02-28 00:00:29 +0530

    Time: 0 ms (100%), Space: 42.2 MB (90.56%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -3,8 +3,7 @@ class Solution {
 
         int count = 0 ; 
         for ( int i = 0 ; i < details.length ; i++ ) {
-            String s = details[i] ;  
-            System.out.println(s.charAt(11) + " " + s.charAt(12)) ; 
+            String s = details[i] ;   
             int num = s.charAt(11) - '0' ;
             int num2 = s.charAt(12) - '0' ;  
             if ( num > 6 || ( num == 6 && num2 > 0)  ) {
