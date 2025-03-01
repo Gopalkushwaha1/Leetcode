a7258ae710e3a2f0f907bb8c7aed178faece7768
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-03-01 11:24:43 +0530

    Time: 1 ms (96.37%), Space: 43.8 MB (44.46%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -6,14 +6,16 @@ class Solution {
                 nums[i+1] = 0 ; 
             }
         }
-        int[] ans = new int[nums.length] ;
-        int i = 0 ;  
+        int idx = 0 ; 
 
-        for  (int ele : nums ) {
-            if ( ele != 0 ) {
-                ans[i++] = ele ; 
+        for ( int i = 0 ; i < nums.length ; i++ ) {
+            if ( nums[i] != 0 ) {
+                int temp = nums[i] ; 
+                nums[i] = nums[idx] ; 
+                nums[idx] = temp;
+                idx++;
             }
-        }
-        return ans ; 
+        } 
+        return nums ; 
     }
 }
