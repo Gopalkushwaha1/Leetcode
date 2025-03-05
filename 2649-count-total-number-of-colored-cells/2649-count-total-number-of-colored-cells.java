21d78387f568acdfa36ee23bfeeae4c6f7f30bdf
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-03-05 16:25:48 +0530

    Time: 0 ms (100%), Space: 40.8 MB (26.23%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,11 +1,5 @@
 class Solution {
     public long coloredCells(int n) {
-        if ( n == 1 ) return 1 ; 
-        long a = 1L * n * n ;
-        n = n - 1 ; 
-        long b = 1L * n * n ; 
-
-        return 1L * (a + b) ; 
-
+        return 1L * n * n + 1L * (n - 1) * (n - 1);
     }
 }
