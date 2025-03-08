900dcb00ae1429b27c73c996d0924a1b0eb111e3
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-03-08 18:24:57 +0530

    Time: 165 ms (6.12%), Space: 45.7 MB (6.51%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.java
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -1,27 +1,27 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         Map<Integer,Integer> map = new HashMap<>() ; 
-
-        for ( int i = 0 ; i < nums1.length ; i++ ) {
-            for ( int j = 0 ; j < nums1.length ; j++ ) {
+        int n = nums1.length , ans = 0 ;  
+        for ( int i = 0 ; i < n ; i++ ) {
+            for ( int j = 0 ; j < n ; j++ ) {
                 int sum = nums1[i] + nums2[j] ; 
-                if ( map.containsKey(sum)){
-                    int freq = map.get(sum) ; 
-                    map.put(sum , freq+1) ;
-                } 
+                if(map.containsKey(sum)){
+                map.put(sum , map.get(sum)+1) ; 
+                }
                 else {
-                    map.put(sum , 1 ) ; 
+                map.put(sum , 1 ) ; 
                 }
             }
         }
-        int ans = 0 ; 
-        for ( int i = 0 ; i < nums3.length ; i++ ) {
-            for ( int j = 0 ; j < nums1.length ; j++ ) {
+        System.out.print(map) ; 
+
+        for ( int i = 0 ; i < n ; i++ ) {
+            for ( int j = 0 ; j < n ; j++ ) {
                 int req = -1 * (nums3[i] + nums4[j]) ; 
-                if(map.containsKey(req)) {
-                    ans += map.get(req) ; 
+                if(map.containsKey(req)){
+                ans += map.get(req) ; 
                 }
-            }
+            } 
         }
         return ans ; 
     }
