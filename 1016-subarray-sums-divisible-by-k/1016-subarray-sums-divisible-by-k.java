1cd268cb63147ba7577bb115aa89cdf1a15753c6
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-03-09 11:45:38 +0530

    Time: 4 ms (90%), Space: 46.8 MB (83.61%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
@@ -3,20 +3,11 @@ class Solution {
         int[] arr = new int[k] ; 
         arr[0] = 1 ; 
         int ans = 0 , sum = 0 ;
-
-        for ( int ele : nums ) {
-            sum += ele ; 
-            sum = sum%k ; 
-            if ( sum < 0 ) {
-                sum += k ; 
-            }
-            if ( arr[sum] > 0 ) {
-                ans += arr[sum] ; 
-                arr[sum]++;
-            }
-            else {
-                arr[sum] = 1 ; 
-            }
+        for ( int ele : nums ) { 
+            sum = (sum+ele)%k ; 
+            if ( sum < 0 ) sum += k ;
+            ans += arr[sum] ; 
+            arr[sum]++;
         } 
         return ans ;  
     }
