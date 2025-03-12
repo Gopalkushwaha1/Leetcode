487ad47efc12e7b7bae77ba028fa4dba47419ea8
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-03-12 14:16:10 +0530

    Time: 8 ms (1.67%), Space: 45.6 MB (8.64%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,12 +1,42 @@
 class Solution {
+    public int positive(int[] nums ) {
+        int start = 0 , end = nums.length -1 ; 
+
+        while  ( start <= end ) {
+            int mid = (start + end) / 2 ; 
+
+            if ( nums[mid] > 0 ) {
+                end = mid - 1 ;  
+            }
+            else {
+                start = mid + 1 ;  
+            }
+        }
+        return start ; 
+    }
+    public int negativeEnd(int[] nums ) {
+        int start = 0 , end = nums.length-1 ; 
+
+        while ( start <= end ) {
+            int mid = (start+end)/2 ; 
+
+            if ( nums[mid] >= 0 ) {
+                end = mid - 1 ; 
+            }
+            else {
+                start = mid + 1 ; 
+            }
+        }
+        return end ; 
+    }
     public int maximumCount(int[] nums) {
-        int zero = 0 , one = 0 , ans = 0 ;
+        int i = 0 , j = nums.length , count = 0 ;
 
-        for ( int ele : nums ) {
-            if ( ele < 0 ) zero++;
-            if ( ele > 0 ) one++ ; 
-        } 
+        int negtaivEnd = negativeEnd(nums) ; 
+        int povStart = positive(nums) ; 
 
-        return Math.max(zero , one ) ; 
+        System.out.print(negtaivEnd + " " + povStart) ; 
+
+        return Math.max ((negtaivEnd+1) , (j-povStart) ) ; 
     }
 }
