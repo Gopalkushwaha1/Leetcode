6fc7282dca29f3c2f1c3dc11528e033e4726cd6e
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-03-15 10:36:55 +0530

    Time: 30 ms (10.45%), Space: 66 MB (6.96%) - LeetHub

---
+++ 2690-house-robber-iv/2690-house-robber-iv.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isPossible( int mid , int[] nums , int k ) {
+        int i = 0 , count = 0 ; 
+
+        while ( i < nums.length ) {
+            if ( nums[i] <= mid ) {
+                count++;
+                i += 2 ; 
+            }
+            else {
+                i++ ; 
+            }
+            if ( count >= k ) return true ; 
+        }
+        return false ; 
+    }
+    public int minCapability(int[] nums, int k) {
+        int min = Integer.MAX_VALUE , max = 0 ; 
+
+        for ( int ele : nums ) {
+            min = Math.min(ele , min ) ; 
+            max = Math.max( ele , max ) ; 
+        } 
+
+        if ( k == 1 ) return  min; 
+
+        while ( min <= max ) {
+            int mid = min + (max - min)/2 ; 
+            System.out.println(min + " " + max + " " + mid ) ; 
+            if ( isPossible(mid , nums , k )) {
+                max = mid - 1 ; 
+            }
+            else {
+                min = mid + 1 ; 
+            }
+        }
+        return min ; 
+    }
+}
