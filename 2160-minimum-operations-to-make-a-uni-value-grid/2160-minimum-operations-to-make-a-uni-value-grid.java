431104723c265bf521a21dcfd4314d3d5b49c23d
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-03-26 22:17:48 +0530

    Time: 37 ms (76.38%), Space: 78 MB (22.11%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
@@ -1,27 +1,30 @@
 class Solution {
-    public int findOperation(int[] arr , int x , int check ) {
-        int req = arr[check] ; 
-        int count = 0 ; 
-        for ( int ele : arr ) { 
-            int diff = Math.abs(req-ele) ; 
-            if ( diff % x != 0 ) return -1 ; 
-            count += diff/x  ; 
-        }
-        return count ;
-    }
-    public int minOperations(int[][] grid, int x) {
-        int[] arr = new int[grid.length*grid[0].length] ;
-        int idx = 0 ;  
-
-        for ( int i = 0 ; i < grid.length ; i++ ) {
-            for ( int j = 0 ; j < grid[0].length ; j++ ) {
-                arr[idx++] = grid[i][j] ; 
+public int minOperations(int[][] grid, int x) {
+        int[] arr = new int[grid.length * grid[0].length];
+        int index = 0;
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                arr[index++] = grid[i][j];
             }
         }
-        Arrays.sort(arr) ; 
-        int oddElement = findOperation(arr,x,arr.length/2) ; 
-
-        return oddElement == -1 ? -1 : oddElement ; 
-
+        
+        Arrays.sort(arr);
+        int median = arr[(arr.length - 1) / 2];
+        int steps = 0;
+        
+        for (int num : arr) {
+            if (num == median) {
+                continue;
+            }
+            
+            if (Math.abs(num - median) % x != 0) {
+                return -1;
+            }
+            
+            steps += (Math.abs(num - median) / x);
+        }
+        
+        return steps;
     }
 }
