82a9183f65e36d69ec117af58613af1088b71abc
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-03-26 22:17:12 +0530

    Time: 40 ms (60.8%), Space: 77.9 MB (22.11%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
@@ -2,29 +2,16 @@ class Solution {
     public int findOperation(int[] arr , int x , int check ) {
         int req = arr[check] ; 
         int count = 0 ; 
-
         for ( int ele : arr ) { 
-            if ( ele == req ) continue ; 
-            else if ( ele < req ) {
-                while ( ele != req ) {
-                    count++;
-                    ele += x ; 
-                    if ( ele > req ) return -1 ; 
-                }
-            }
-            else {
-                while ( ele != req ) {
-                    count++;
-                    ele -= x ;
-                    if ( ele < 0 ) return -1 ; 
-                }
-            }
+            int diff = Math.abs(req-ele) ; 
+            if ( diff % x != 0 ) return -1 ; 
+            count += diff/x  ; 
         }
         return count ;
     }
     public int minOperations(int[][] grid, int x) {
         int[] arr = new int[grid.length*grid[0].length] ;
-        int ans = 0 , idx = 0 ;  
+        int idx = 0 ;  
 
         for ( int i = 0 ; i < grid.length ; i++ ) {
             for ( int j = 0 ; j < grid[0].length ; j++ ) {
@@ -32,14 +19,6 @@ class Solution {
             }
         }
         Arrays.sort(arr) ; 
-        // if ( arr.length % 2 == 0 ) {
-        //     int left = findOperation(arr , x , (arr.length-1)/2) ; 
-        //     int right = findOperation(arr , x , arr.length/2) ;
-        //     if ( left == -1 || right == -1 ) {
-        //         return -1 ; 
-        //     }
-        //     return Math.min(left , right ) ; 
-        // }
         int oddElement = findOperation(arr,x,arr.length/2) ; 
 
         return oddElement == -1 ? -1 : oddElement ; 
