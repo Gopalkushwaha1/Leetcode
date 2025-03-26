6034f29c9361cd12d9e2c5257211d683ba29e4c2
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-03-26 21:59:28 +0530

    Time: 853 ms (7.54%), Space: 77.8 MB (28.64%) - LeetHub

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int findOperation(int[] arr , int x , int check ) {
+        int req = arr[check] ; 
+        int count = 0 ; 
+
+        for ( int ele : arr ) { 
+            if ( ele == req ) continue ; 
+            else if ( ele < req ) {
+                while ( ele != req ) {
+                    count++;
+                    ele += x ; 
+                    if ( ele > req ) return -1 ; 
+                }
+            }
+            else {
+                while ( ele != req ) {
+                    count++;
+                    ele -= x ;
+                    if ( ele < 0 ) return -1 ; 
+                }
+            }
+        }
+        return count ;
+    }
+    public int minOperations(int[][] grid, int x) {
+        int[] arr = new int[grid.length*grid[0].length] ;
+        int ans = 0 , idx = 0 ;  
+
+        for ( int i = 0 ; i < grid.length ; i++ ) {
+            for ( int j = 0 ; j < grid[0].length ; j++ ) {
+                arr[idx++] = grid[i][j] ; 
+            }
+        }
+        Arrays.sort(arr) ; 
+        if ( arr.length % 2 == 0 ) {
+            int left = findOperation(arr , x , (arr.length-1)/2) ; 
+            int right = findOperation(arr , x , arr.length/2) ;
+            if ( left == -1 || right == -1 ) {
+                return -1 ; 
+            }
+            return Math.min(left , right ) ; 
+        }
+        int oddElement = findOperation(arr,x,arr.length/2) ; 
+
+        return oddElement == -1 ? -1 : oddElement ; 
+
+    }
+}
