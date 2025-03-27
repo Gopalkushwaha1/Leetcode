3a0aa74ded8a77527c104e0285d2f5e05ecbd4d5
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-03-27 11:18:02 +0530

    Time: 566 ms (5.13%), Space: 60.2 MB (25%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minimumIndex(List<Integer> nums) {
+        Map<Integer,Integer> map = new HashMap<>() ; 
+        int maxEle = 0 , count = 0 ; 
+
+        for ( int i = 0 ; i < nums.size() ; i++ ) {
+            map.put(nums.get(i) , map.getOrDefault(nums.get(i) , 0 )+ 1) ; 
+        }
+
+        for ( Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if ( count < entry.getValue()){
+                maxEle = entry.getKey();
+                count = entry.getValue();
+            }
+        }
+        int[] ans = new int[nums.size()]; 
+        count = 0 ; 
+        for ( int i = 0 ; i < nums.size() ; i++ ) {
+            if ( nums.get(i) == maxEle) {
+                ans[i] = ++count;
+            }
+            else {
+                ans[i] = count ; 
+            }
+        }
+        for ( int i = 0 ; i < ans.length-1 ; i++ ) { 
+            int left = ans[i]*2 ;
+            int right = (ans[ans.length-1] - ans[i])*2 ; 
+            System.out.println(left + " " + right) ; 
+            if(left > i+1 && right > ans.length - (i+1) ) return i ; 
+        } 
+        return -1; 
+    }
+}
