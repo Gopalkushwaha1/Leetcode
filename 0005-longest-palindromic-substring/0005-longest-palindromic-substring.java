601f7e862e60ba9405f14555f62b23de70fb6993
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-04-04 10:48:27 +0530

    Time: 272 ms (22.82%), Space: 55.2 MB (5.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,24 +1,29 @@
 class Solution {
-    public boolean isPalindrom(String s , int i , int j ) {
-        while ( i < j ) {
-            if(s.charAt(i) == s.charAt(j)){
-                i++;
-                j--;
-            }
-            else {
-                return false ; 
-            }
+    public boolean isPalindrom(String s , int i , int j , int[][] check) {
+        if ( i >= j ) {
+           return true ; 
+        } 
+
+        if ( check[i][j] != -1 ) return check[i][j] == 1 ; 
+
+        if(s.charAt(i) == s.charAt(j) && isPalindrom(s,i+1,j-1,check)) {
+            return (check[i][j] = 1) == 1 ; 
         }
-        return true ; 
+
+        return (check[i][j] = 0) == 1 ; 
     }
 
     public String longestPalindrome(String s) {
         int startIdx = 0 ; 
         int palindromicMaxLen = 0 ; 
+        int[][] check = new int[s.length()][s.length()];
+        for ( int i = 0 ; i < s.length() ; i++ ) {
+            Arrays.fill(check[i] , -1 ) ; 
+        }
 
         for (int i = 0 ; i < s.length() ; i++ ) {
             for ( int j = i ; j < s.length() ; j++ ) {
-                if(isPalindrom(s , i , j ) && palindromicMaxLen < j - i + 1){
+                if(isPalindrom(s , i , j , check) && palindromicMaxLen < j - i + 1){
                     startIdx = i ; 
                     palindromicMaxLen = j - i + 1 ; 
                 }
