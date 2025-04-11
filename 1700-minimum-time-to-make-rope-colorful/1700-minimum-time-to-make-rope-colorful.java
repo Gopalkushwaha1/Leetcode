888ed202805b03b29a06eff9391c6425cb3976bf
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-04-11 09:36:20 +0530

    Time: 11 ms (26.22%), Space: 61 MB (26.75%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.java
@@ -1,25 +1,21 @@
 class Solution {
-    public int minCost(String colors, int[] need) {
-        Stack<Character> st = new Stack<>() ; 
-        Stack<Integer> st2 = new Stack<>() ; 
+    public int minCost(String colors, int[] neededTime) {
         int ans = 0 ; 
-        char[] ch = colors.toCharArray();
-        st.push(ch[0]);
-        st2.push(need[0]);
-        for ( int i = 1 ; i < colors.length() ; i++ ) {
-            char c = ch[i] ; 
-            char top = st.peek() ; 
-            if(top == c && need[i] <= st2.peek()) {
-                ans += need[i] ; 
-                continue;
-            }
-            else if( top == c && need[i] > st2.peek()){
-                st.pop() ; 
-                ans += st2.pop() ;
-            }
-            st.add(c);
-            st2.add(need[i]) ; 
-        } 
+
+        for ( int i = 0 ; i < colors.length()-1 ; i++ ) {
+            if(colors.charAt(i) != colors.charAt(i+1)) continue ; 
+            int j = i ,  sum = 0 , max = 0 ; 
+            while ( j < colors.length() - 1 && colors.charAt(j) == colors.charAt(j+1) ) {
+                sum += neededTime[j] ; 
+                max = Math.max(max , neededTime[j]) ; 
+                i++;
+                j++; 
+            } 
+            sum += neededTime[j];
+            max = Math.max(max , neededTime[j]) ; 
+            ans += (sum - max) ; 
+        }
+
         return ans ; 
     }
 }
