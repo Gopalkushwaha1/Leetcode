a94676411de10c5839816f094b164632530310af
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-04-19 17:30:57 +0530

    Time: 6 ms (35.32%), Space: 55.9 MB (37.61%) - LeetHub

---
+++ 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long minimumReplacement(int[] nums) {
+        long operation = 0 ; 
+
+        for ( int i = nums.length - 2 ; i >= 0 ; i-- ) {
+            if( nums[i] > nums[i+1]) {
+                int count = nums[i]/nums[i+1] ; 
+
+                if(nums[i] % nums[i+1] != 0 ) {
+                    count++;
+                }
+                operation += count -1 ; 
+                nums[i] = (nums[i]/count) ; 
+            }
+        } 
+        return operation ; 
+    }
+}
