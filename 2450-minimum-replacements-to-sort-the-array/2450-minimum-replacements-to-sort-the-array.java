96416ca25aad4593c1d7f910e4db8b4a91ad634e
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-04-19 17:32:05 +0530

    Time: 5 ms (56.88%), Space: 55.8 MB (63.76%) - LeetHub

--- 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.java
+++ 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.java
@@ -3,15 +3,16 @@ class Solution {
         long operation = 0 ; 
 
         for ( int i = nums.length - 2 ; i >= 0 ; i-- ) {
-            if( nums[i] > nums[i+1]) {
-                int count = nums[i]/nums[i+1] ; 
+            if( nums[i] < nums[i+1]) {
+                continue ;  
+            }
+            int count = nums[i]/nums[i+1] ; 
 
                 if(nums[i] % nums[i+1] != 0 ) {
                     count++;
                 }
                 operation += count -1 ; 
-                nums[i] = (nums[i]/count) ; 
-            }
+                nums[i] = (nums[i]/count) ;
         } 
         return operation ; 
     }
