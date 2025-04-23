d0fd5337afb11f5c6ed171a44fed015f5faaebca
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-04-23 11:27:02 +0530

    Time: 0 ms (100%), Space: 42.7 MB (82.35%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void findAll( int i , int n ,List<List<Integer>> ans ,ArrayList<Integer> result , int arr[]  ){
+    public void findAll( int i , int n ,ArrayList<List<Integer>> ans ,ArrayList<Integer> result , int arr[]  ){
         if( i == n ) {
             ans.add(new ArrayList(result));
             return ; 
@@ -10,7 +10,7 @@ class Solution {
         findAll(i+1 , n , ans , result , arr) ; 
     }
     public List<List<Integer>> subsets(int[] arr) {
-        List<List<Integer>> ans = new ArrayList<>() ; 
+        ArrayList<List<Integer>> ans = new ArrayList<>() ; 
         ArrayList<Integer> result = new ArrayList<>() ; 
         findAll(0,arr.length , ans , result , arr ) ; 
         return ans ;
