f6eb98870c06d932a0e86b24fafbac95cbc0a0f4
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-09-08 16:29:22 +0530

    Time: 135 ms (19.72%), Space: 47.1 MB (43.35%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        p = []
+        n = [] 
+
+        for i in  range(len(nums)) :
+            if nums[i] >= 0 :
+                p.append(nums[i])
+            else :
+                n.append(nums[i])
+        for i in range(len(nums)) :
+            if i % 2 == 0 :
+                nums[i] = p[i/2] 
+            else :
+                nums[i] = n[i/2] 
+
+        return nums                         
