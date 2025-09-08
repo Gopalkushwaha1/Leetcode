8d60a826fa648af763d2df2b302ce656e9716aff
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-09-08 16:18:13 +0530

    Time: 98 ms (42.64%), Space: 19.5 MB (22.84%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -5,16 +5,11 @@ class Solution(object):
         :rtype: int
         """
         curr_min = prices[0] 
-        curr_max = prices[0] 
-
         ans = 0 
 
-        for i in range(1,len(prices)) :
-            if prices[i] < curr_min :
-                curr_min = prices[i]
-                curr_max = prices[i] 
-            if prices[i] > curr_max :
-                curr_max = prices[i] 
-                ans = max(ans , curr_max - curr_min)  
+        for i in range(len(prices)) :
+             if prices[i] < curr_min :
+                curr_min = prices[i] 
+             ans = max(ans , prices[i] - curr_min)     
 
         return ans           
