52a4599e775c4cf286a792c4e23425cbeb7a80e4
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-10-31 12:32:42 +0530

    Time: 1 ms (100%), Space: 44.9 MB (43.32%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -1,20 +1,27 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        HashSet<Integer> set = new HashSet<>() ; 
-        List<Integer> list = new ArrayList<>() ; 
-
-        for ( int ele : nums ) {
-            if(set.contains(ele)) {
-                list.add(ele) ; 
-                continue ; 
+        int[] ans = new int[2] ; 
+        int j = 0 ; 
+        for ( int i = 0 ; i < nums.length ; i++ ) {
+            if(nums[i] == 0 ) {
+                nums[i] = nums.length ; 
             }
-            set.add(ele) ; 
         }
 
-        int[] ans = new int[2] ; 
-        ans[0] = list.get(0) ; 
-        ans[1] = list.get(1) ; 
-
+        for ( int i = 0 ; i < nums.length ; i++ ) {
+            int idx = Math.abs(nums[i]) ; 
+            if(nums[idx%nums.length] < 0 ) {
+                if ( idx == nums.length ) {
+                    ans[j++] = 0 ; 
+                    continue ; 
+                }
+                ans[j++] = idx ; 
+                continue ;  
+            }
+            else {
+                nums[idx%nums.length] = -1*nums[idx%nums.length] ; 
+            }
+        }
         return ans ; 
     }
 }
