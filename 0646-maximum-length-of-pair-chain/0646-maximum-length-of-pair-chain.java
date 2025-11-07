0ebe6ddbfd6e172f0bc85535e203c782ac3fb2e1
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-11-07 14:57:29 +0530

    Time: 8 ms (99.65%), Space: 47.2 MB (12.09%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,31 +1,29 @@
 class Solution {
-    public int ans(int[][] arr , int curr , int prev , int[][] dp ) {
-        // base case 
-        if( curr == arr.length ) {
-            return 0 ; 
-        }
-
-        if ( dp[curr][prev+1] != -1 ) return dp[curr][prev+1] ; 
-        int skip = ans(arr , curr+1 , prev , dp ) ; 
-
-        int take = 0 ; 
-        if ( prev == -1 || ( arr[curr][0] > arr[prev][1])) {
-            take = 1 + ans(arr , curr+1 , curr , dp ) ; 
-        }
-
-        return dp[curr][prev+1] =  Math.max(skip , take ) ; 
-    }
     public int findLongestChain(int[][] pairs) {
-        Arrays.sort(pairs , (a,b) -> {
-            if ( a[0] == b[0] ) 
-                return b[0] - a[0] ;  
-            return a[0] - b[0]  ;   
-        });
-        int[][] dp = new int[pairs.length][pairs.length+1] ; 
+        int len = pairs.length ; 
 
-        for (  int i = 0 ; i < pairs.length ; i++ ) {
-            Arrays.fill(dp[i] , -1) ; 
+        // Arrays.sort(pairs , (a,b) -> {
+        //     if( a[0] == b[0] ) 
+        //         return b[0] - a[0] ;
+        //     return a[1] - b[1] ;     
+        // });
+
+        Arrays.sort(pairs,new Comparator<int[]>(){
+        public int compare(int[] a,int[] b){
+            return a[1]-b[1];
         }
-        return ans(pairs , 0 , -1 , dp ) ; 
+      } );
+
+        int val = pairs[0][1] ; 
+        int count = 1 ; 
+
+        for ( int i = 1 ; i < len ; i++ ) {
+
+            if( pairs[i][0] > val ) {
+                count++ ; 
+                val = pairs[i][1] ; 
+            }
+        }
+        return count ; 
     }
 }
