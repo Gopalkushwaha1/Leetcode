1c98886f60f0f12a90c19969344fa2bd035228bc
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-11-18 10:36:12 +0530

    Time: 4 ms (11.69%), Space: 45.8 MB (12.41%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,21 +1,21 @@
 class Solution {
-    public void ans(int[] arr , int target , int sum , int idx , List<List<Integer>> result , List<Integer> check ) {
+    public void ans(int[] arr , int target , int idx , List<List<Integer>> result , List<Integer> check ) {
 
-        if ( sum == target ) {
+        if (target == 0  ) {
             result.add(new ArrayList<>(check)) ;  
             return ; 
         }
-        
+
         // base case 
         if( idx == arr.length || arr[idx] > target  ) {
             return ; 
         }
 
         
-        ans(arr , target , sum , idx+1 , result , check ) ;
-        if ( arr[idx] + sum <= target ) {
+        ans(arr , target , idx+1 , result , check ) ;
+        if ( arr[idx] <= target ) {
             check.add(arr[idx]) ; 
-            ans(arr , target , sum + arr[idx] , idx , result , check ) ;
+            ans(arr , target - arr[idx] , idx , result , check ) ;
             check.remove(check.size() - 1 ) ;  
         }
          
@@ -23,7 +23,7 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates) ;
         List<List<Integer>> result = new ArrayList<>()  ; 
-        ans(candidates , target , 0 , 0 ,result  , new ArrayList<>() ) ; 
+        ans(candidates , target , 0 ,result  , new ArrayList<>() ) ; 
         return result ; 
     }
 }
