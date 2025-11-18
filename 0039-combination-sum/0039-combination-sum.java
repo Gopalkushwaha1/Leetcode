14ce3528a0c8fdded056030a8f281e4332a7021f
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-11-18 09:41:17 +0530

    Time: 9 ms (5.51%), Space: 47.3 MB (7.87%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void ans(int[] arr , int target , int sum , int idx , List<List<Integer>> result , List<Integer> check ) {
+        // base case 
+        if( idx == arr.length || sum > target ) {
+            return ; 
+        }
+
+        if ( sum == target ) {
+            result.add(check) ; 
+            return ; 
+        }
+
+        ans(arr , target , sum , idx+1 , result , check ) ; 
+        List<Integer> list = new ArrayList<>(check) ; 
+        list.add(arr[idx]) ; 
+        ans(arr , target , sum + arr[idx] , idx , result , list ) ; 
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates) ;
+        List<List<Integer>> result = new ArrayList<>()  ; 
+        ans(candidates , target , 0 , 0 ,result  , new ArrayList<>() ) ; 
+        return result ; 
+    }
+}
