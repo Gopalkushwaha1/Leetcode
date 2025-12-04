6b4aeb07093a627916955092c31c25a49d7ed702
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-12-04 10:34:35 +0530

    Time: 10 ms (93.15%), Space: 47.7 MB (26.61%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
@@ -1,47 +1,24 @@
 class Solution {
-    public int countCollisions(String d) {
-        Stack<Character> st = new Stack<>() ; 
 
-        int i = 0 ; 
-        int count = 0 ; 
+    public int countCollisions(String directions) {
+        int n = directions.length();
+        int l = 0;
+        int r = n - 1;
 
-        while ( i < d.length() ) {
-            char ch = d.charAt(i) ; 
-            if(st.isEmpty() || d.charAt(i) == 'R') { 
-                st.push(ch) ; 
-                i++ ; 
-                continue ; 
-            }
-
-            else if ( ch == 'L' ) {
-                if(st.peek() == 'S') {
-                    count++ ; 
-                }
-                else if(st.peek() == 'R') {
-                    count += 2 ; 
-                    st.pop() ; 
-                    while ( !st.isEmpty() && st.peek() == 'R') {
-                        count++ ; 
-                        st.pop() ; 
-                    }
-                    st.push('S') ; 
-                }
-                else {
-                    st.push(ch) ; 
-                }
-            }
-
-            else {
-                if( st.peek() == 'R') {
-                    while ( !st.isEmpty() && st.peek() == 'R') {
-                        count++ ; 
-                        st.pop() ; 
-                    }
-                }
-                st.push('S') ; 
-            }
-            i++ ; 
+        while (l < n && directions.charAt(l) == 'L') {
+            l++;
         }
-        return count ; 
+
+        while (r >= l && directions.charAt(r) == 'R') {
+            r--;
+        }
+
+        int res = 0;
+        for (int i = l; i <= r; i++) {
+            if (directions.charAt(i) != 'S') {
+                res++;
+            }
+        }
+        return res;
     }
 }
