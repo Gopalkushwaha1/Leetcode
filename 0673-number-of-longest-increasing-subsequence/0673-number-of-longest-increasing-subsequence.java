b527d4c77d0b399ca830b2a2906ccaf58d478f9b
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-12-09 09:44:34 +0530

    Time: 31 ms (7.67%), Space: 45.8 MB (8.6%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
@@ -27,8 +27,7 @@ class Solution {
             }
         }
 
-        for ( int i = 0 ; i < nums.length ; i++ ) {
-            System.out.println("maxLen -> " + maxLen[i] + " Count -> " + count[i]) ; 
+        for ( int i = 0 ; i < nums.length ; i++ ) { 
             if(maxLen[i] == len ) {
                 ans += count[i] ; 
             }
