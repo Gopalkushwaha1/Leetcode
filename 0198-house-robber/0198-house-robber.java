8e0ef8a5de019e00dde55cc052d803da86f24ec6
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-12-10 08:32:26 +0530

    Time: 0 ms (100%), Space: 43 MB (6.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,16 @@
 class Solution {
-    public int ans(int[] nums, int idx , int[] dp  ) {
-        if( idx >= nums.length ) {
-            return 0 ; 
-        }
-        if(dp[idx] != 0 ) return dp[idx] ;
-        int take = nums[idx] + ans(nums , idx+2 , dp ) ; 
-        int skip = ans(nums,idx+1 , dp ) ; 
-         
+    public int maxRob(int[] nums , int idx , int[] dp ) {
+        if ( idx >= nums.length ) return 0 ; 
 
-        return dp[idx] = Math.max(skip , take) ; 
+        if ( dp[idx] != -1 ) return dp[idx] ; 
+        int skip = maxRob(nums , idx + 1 , dp ) ; 
+        int take = nums[idx] + maxRob(nums , idx + 2 , dp  ) ; 
+
+        return dp[idx] =  Math.max(skip , take ) ; 
     }
     public int rob(int[] nums) {
         int[] dp = new int[nums.length] ; 
-        return ans(nums , 0 , dp ) ; 
+        Arrays.fill(dp,-1) ; 
+        return maxRob(nums , 0 , dp  ) ; 
     }
 }
