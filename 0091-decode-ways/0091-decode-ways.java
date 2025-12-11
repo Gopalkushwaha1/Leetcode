7dc70bb44115ac59c13e70eb7a9f6b20bd7a05d3
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-12-11 08:46:19 +0530

    Time: 1 ms (85.23%), Space: 43.1 MB (40.74%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length()+1] ; 
+        if ( s == null || s.charAt(0) == '0') return 0 ; 
+
+        dp[0] = 1 ; 
+        dp[1] = 1 ; 
+
+        for ( int i = 2 ; i <= s.length() ; i++ ) {
+            char curr = s.charAt(i-1) ; 
+            char prev = s.charAt(i-2) ;
+
+            if ( curr != '0') {
+                dp[i] += dp[i-1] ; 
+            } 
+
+            if ( prev == '1' || ( prev == '2' && curr <= '6')){
+                dp[i] += dp[i-2] ; 
+            }
+        }
+
+        return dp[s.length()] ; 
+    }
+}
