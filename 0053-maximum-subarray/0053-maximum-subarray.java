624e389102ef8e9803193f3de40adf31d8f56fb8
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-12-14 10:40:14 +0530

    Time: 1 ms (99.76%), Space: 77 MB (55.55%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,12 +4,13 @@ class Solution {
         int localSum = nums[0] ; 
 
         for ( int i = 1 ; i < nums.length ; i++  ) {
-            int ele = nums[i] ; 
+            int ele = nums[i] ;
+            if ( localSum < 0 ) localSum = 0 ; 
             localSum += ele ; 
             if ( maxSum < localSum ) {
                 maxSum = localSum ; 
             }
-            if ( localSum < 0 ) localSum = 0 ; 
+            
         }
         return maxSum ; 
     }
