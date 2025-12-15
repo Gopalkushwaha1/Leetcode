6e4dbd31d366d2b6a80d0628c99db2d82cf248cf
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-12-15 09:51:55 +0530

    Time: 0 ms (100%), Space: 48.2 MB (56.27%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,13 +1,14 @@
 class Solution {
     public void reverseString(char[] s) {
-        // define prerequistite Variable 
-        int i = 0 , n = s.length ; 
-        // swap first to last char 
-        while ( i < n ) {
-            char c = s[i] ; 
-            s[i] = s[n-1] ;
-            s[n-1] = c ; 
-            i++ ; n-- ; 
+        int i = 0 ; 
+        int j = s.length - 1 ; 
+
+        while ( i < j ) {
+            char ch = s[i] ; 
+            s[i] = s[j] ; 
+            s[j] = ch ; 
+            i++ ; 
+            j-- ;  
         }
     }
 }
