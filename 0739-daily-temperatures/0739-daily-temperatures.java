01ede58a9579fb410930d1434527a488a1c0b55c
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-12-18 09:11:19 +0530

    Time: 83 ms (7.6%), Space: 107.9 MB (9.57%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -5,10 +5,6 @@ class Solution {
         Stack<Integer> st = new Stack<>() ; 
 
         for ( int i = 0 ; i < t.length ; i++ ) {
-            if ( st.isEmpty() || t[st.peek()] > t[i]) {
-                st.push(i) ; 
-                continue ; 
-            }
             while(!st.isEmpty() && t[st.peek()] < t[i]) {
                 ans[st.peek()] = i - st.peek() ; 
                 st.pop() ; 
