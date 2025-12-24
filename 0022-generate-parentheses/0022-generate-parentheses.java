cd56adf541bde49de96f53b94d5dbe906296f962
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-12-24 09:29:45 +0530

    Time: 1 ms (84.4%), Space: 45.4 MB (8.48%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,19 +1,20 @@
 class Solution {
-    public void find(List<String> ans , int n , int s , int c , String str ) {
-        if( str.length() == 2*n ) {
-            ans.add(str) ;
+    public void getAll(StringBuilder sb , int open , int close , int n , List<String> list ) {
+        if(close > open || open > n ) return ; 
+        if(open + close == 2*n) {
+            list.add(sb.toString()) ; 
             return ; 
         }
-        if( s > 0 ) {
-            find(ans ,n, s-1 , c , str + "(");
-        }
-        if (( s - c ) < 0 ) 
-            find( ans ,n, s , c-1 , str + ")") ; 
+        StringBuilder sb1 = new StringBuilder(sb) ; 
+        StringBuilder sb2 = new StringBuilder(sb) ; 
+        getAll(sb1.append("(") , open+1 , close , n , list ) ; 
+        getAll(sb2.append(")") , open , close+1 , n , list ) ; 
     }
     public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList<>() ; 
-
-        find( ans , n ,  n ,n ,  "") ; 
-        return ans ; 
-    }
+        StringBuilder sb = new StringBuilder() ; 
+        sb.append("(") ; 
+        List<String> list = new ArrayList<>() ; 
+        getAll(sb , 1 , 0 , n , list ) ; 
+        return list ; 
+     }
 }
