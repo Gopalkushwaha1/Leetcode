b1a0f4b7c24eeb836407206153ccb1de0e672b28
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2025-12-25 11:57:50 +0530

    Time: 3 ms (51.81%), Space: 48 MB (16.02%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,34 +1,114 @@
 class Solution {
+    public boolean isValid2(int s , char[][] board ) {
+        HashSet<Character> set = new HashSet<>() ; 
+        for ( int i = 0 ; i < 9 ; i++ ) {
+            char ch = board[s][i] ; 
+            if ( ch == '.') continue ; 
+            if(set.contains(ch)) return false ; 
+            set.add(board[s][i]) ;  
+        }
+        return true ; 
+    }
+    public boolean isValid(int s ,char[][] board ) {
+        HashSet<Character> set = new HashSet<>() ; 
+        for ( int i = 0 ; i < 9 ; i++ ) {
+            char ch = board[i][s] ; 
+            if ( ch == '.') continue ; 
+            if(set.contains(ch)) return false ; 
+            set.add(board[i][s]) ;  
+        }
+        return true ; 
+    }
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Character>[] rows = new HashSet[9];
-        HashSet<Character>[] cols = new HashSet[9];
-        HashSet<Character>[] boxes = new HashSet[9];
-
-        for (int i = 0; i < 9; i++) {
-            rows[i] = new HashSet<>();
-            cols[i] = new HashSet<>();
-            boxes[i] = new HashSet<>();
+        boolean check = true ; 
+        for ( int i = 0 ; i < 9 ; i++ ) {
+            check =  isValid(i,board) ;   // col
+            if ( !check ) return false ;
+            check = isValid2(i,board) ;   // row 
+            if ( !check ) return false ; 
         }
 
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
-                if (board[r][c] == '.') {
-                    continue;
-                }
-
-                char value = board[r][c];
-                int boxIndex = (r / 3) * 3 + (c / 3);
-
-                if (rows[r].contains(value) || cols[c].contains(value) || boxes[boxIndex].contains(value)) {
-                    return false;
-                }
-
-                rows[r].add(value);
-                cols[c].add(value);
-                boxes[boxIndex].add(value);
+        HashSet<Character> set = new HashSet<>() ; 
+        for ( int i = 0 ; i <= 2 ; i++ ) {
+            for ( int j = 0 ; j <= 2 ; j++ ) {
+                if(board[i][j] == '.') continue ; 
+                if(set.contains(board[i][j])) return false ; 
+                set.add(board[i][j]) ; 
             }
         }
 
-        return true;        
+        HashSet<Character> set2 = new HashSet<>() ; 
+        for ( int i = 0 ; i <= 2 ; i++ ) {
+            for ( int j = 3 ; j <= 5 ; j++ ) {
+                if(board[i][j] == '.') continue ; 
+                if(set2.contains(board[i][j])) return false ; 
+                set2.add(board[i][j]) ; 
+            }
+        }
+
+        HashSet<Character> set3 = new HashSet<>() ; 
+        for ( int i = 0 ; i <= 2 ; i++ ) {
+            for ( int j = 6 ; j <= 8 ; j++ ) {
+                if(board[i][j] == '.') continue ; 
+                if(set3.contains(board[i][j])) return false ; 
+                set3.add(board[i][j]) ; 
+            }
+        }
+
+        HashSet<Character> set4 = new HashSet<>() ; 
+        for ( int i = 3 ; i <=5 ; i++ ) {
+            for ( int j = 0 ; j <= 2 ; j++ ) {
+                if(board[i][j] == '.') continue ; 
+                if(set4.contains(board[i][j])) return false ; 
+                set4.add(board[i][j]) ; 
+            }
+        }
+
+        HashSet<Character> set5 = new HashSet<>() ; 
+        for ( int i = 3 ; i <= 5 ; i++ ) {
+            for ( int j = 3 ; j <= 5 ; j++ ) {
+                if(board[i][j] == '.') continue ; 
+                if(set5.contains(board[i][j])) return false ; 
+                set5.add(board[i][j]) ; 
+            }
+        }
+
+        HashSet<Character> set6 = new HashSet<>() ; 
+        for ( int i = 3 ; i <= 5 ; i++ ) {
+            for ( int j = 6 ; j <= 8 ; j++ ) {
+                if(board[i][j] == '.') continue ; 
+                if(set6.contains(board[i][j])) return false ; 
+                set6.add(board[i][j]) ; 
+            }
+        }
+
+        HashSet<Character> set7 = new HashSet<>() ; 
+        for ( int i = 6 ; i <= 8 ; i++ ) {
+            for ( int j = 0 ; j <= 2 ; j++ ) {
+                if(board[i][j] == '.') continue ; 
+                if(set7.contains(board[i][j])) return false ; 
+                set7.add(board[i][j]) ; 
+            }
+        }
+
+        HashSet<Character> set8 = new HashSet<>() ; 
+        for ( int i = 6 ; i <= 8 ; i++ ) {
+            for ( int j = 3 ; j <= 5 ; j++ ) {
+                if(board[i][j] == '.') continue ; 
+                if(set8.contains(board[i][j])) return false ; 
+                set8.add(board[i][j]) ; 
+            }
+        }
+
+        HashSet<Character> set9 = new HashSet<>() ; 
+        for ( int i = 6 ; i <= 8 ; i++ ) {
+            for ( int j = 6 ; j <= 8 ; j++ ) {
+                if(board[i][j] == '.') continue ; 
+                if(set9.contains(board[i][j])) return false ; 
+                set9.add(board[i][j]) ; 
+            }
+        }
+
+        return true ; 
     }
 }
