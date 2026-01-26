507e6e437825c5fb0ee618408297679f0d55df72
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2026-01-26 16:12:17 +0530

    Time: 16 ms (46.46%), Space: 92.9 MB (68.49%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,20 +1,11 @@
 class Solution {
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-         }
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>() ; 
 
         for ( int ele : nums ) {
-            if ( set.contains(ele)) return true ; 
+            if(set.contains(ele)) return true ; 
             set.add(ele) ; 
         }
-
         return false ; 
     }
 }
