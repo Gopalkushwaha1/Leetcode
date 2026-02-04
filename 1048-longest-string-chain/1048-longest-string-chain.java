b6eb2b31684b5ac511461986da295a9da947ae04
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2026-02-05 00:59:30 +0530

    Time: 40 ms (81.88%), Space: 47 MB (34.91%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,45 +1,25 @@
 class Solution {
-    public boolean isValid(String str1 , String str2 ) {
-        if ( str2.length() - str1.length() != 1 ) return false ; 
-
-        int s = 0 ; 
-        int e = 0 ; 
-        int count = 0 ; 
-
-        while ( s < str1.length() && e < str2.length() ) {
-            if(str1.charAt(s) == str2.charAt(e)) {
-                s++ ; 
-                e++ ; 
-                continue ; 
-            }
-            else {
-                count++ ; 
-                e++ ; 
-                if ( count > 1 ) return false ; 
-            }
-        }
-        return true ; 
-    }
-    public int ans( String[] word , int prev , int curr , int[][] dp ) {
-        if ( curr == word.length ) {
-            return 0 ; 
-        }
-        if(dp[prev+1][curr] != -1 ) return dp[prev+1][curr] ; 
-        int skip = ans(word , prev , curr+1 , dp ) ; 
-        int take = 0 ; 
-
-        if ( prev == -1 || isValid(word[prev] , word[curr])) {
-            take = 1 + ans(word , curr , curr + 1 , dp ) ; 
-        }
-
-        return dp[prev+1][curr] =  Math.max(skip , take ) ; 
-    }
     public int longestStrChain(String[] words) {
-        Arrays.sort(words , ( a,b) -> a.length() - b.length()) ; 
-        int[][] dp = new int[words.length][words.length] ; 
-        for ( int i = 0 ; i < words.length ; i++ ) {
-            Arrays.fill(dp[i] , -1 ) ; 
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        Map<String, Integer> dp = new HashMap<>();
+        
+        int max = 1;
+        
+        for (String word : words) {
+            int best = 1;  
+            
+            for (int i = 0; i < word.length(); i++) {
+                String prev = word.substring(0, i) + word.substring(i + 1);
+                
+                if (dp.containsKey(prev)) {
+                    best = Math.max(best, dp.get(prev) + 1);
+                }
+            }
+            
+            dp.put(word, best);
+            max = Math.max(max, best);
         }
-        return ans(words , -1 , 0 , dp ) ; 
+        
+        return max;
     }
 }
