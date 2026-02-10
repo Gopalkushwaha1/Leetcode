2d2318d381d76fb73a3bc023cd177d7340315b65
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2026-02-10 10:11:01 +0530

    Time: 24 ms (100%), Space: 47.5 MB (11.31%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
@@ -1,14 +1,31 @@
 class Solution {
     public int longestBalanced(int[] nums) {
        int ans = 0 ; 
+       int[] seenOdd = new int[100001] ;
+       int[] seenEven = new int[100001] ;
+       int track = 0 ; 
        for ( int i = 0 ; i < nums.length ; i++ ) {
-            Set<Integer> odd = new HashSet<>() ;
-            Set<Integer> even = new HashSet<>() ;
+            if(ans >= nums.length - i ) break ; 
+            track++ ; 
+            int even = 0 ; 
+            int odd = 0 ; 
          for ( int j = i ; j < nums.length ; j++ ) {
-                if(nums[j] % 2 == 0 ) even.add(nums[j]) ; 
-                else odd.add(nums[j]) ; 
-                if(even.size() == odd.size() ) ans = Math.max(ans , j - i + 1 ) ; 
-         }
+                int val = nums[j] ; 
+
+                if((val & 1) == 1 ) {
+                    if(seenEven[val] != track ) {
+                        seenEven[val] = track ; 
+                        even++ ; 
+                    }
+                }    
+                else {
+                    if(seenOdd[val] != track ) {
+                        seenOdd[val] = track ; 
+                        odd++ ; 
+                    }
+                }
+                if(even == odd ) ans = Math.max(ans , j - i + 1 ) ;  
+            }
        } 
        return ans ; 
     }
