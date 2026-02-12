175bde645716138de3073dbbbce0a0d7adde9d16
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2026-02-12 08:39:49 +0530

    Time: 130 ms (65.17%), Space: 46.7 MB (89.7%) - LeetHub

---
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(int[] freq , int n) {
+        for ( int ele : freq ) {
+            if(ele == 0 ) continue ; 
+            if(ele != n ) return false ; 
+        }
+        return true ; 
+    }
+    public int longestBalanced(String s) {
+        int maxLen = 0 ;
+        int len = s.length() ;  
+
+        for ( int i = 0 ; i < len ; i++ ) {
+            int[] freq = new int[26] ; 
+            for ( int j = i ; j < len ; j++ ) {
+                freq[s.charAt(j) - 'a']++ ; 
+                if(isValid(freq , freq[s.charAt(j) - 'a'])) maxLen = Math.max(maxLen , j - i + 1 ) ; 
+            }
+        }
+        return maxLen ; 
+    }
+}
