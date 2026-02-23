92aeec8a3c792fb0510e3f9db8781c19cfe5db28
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2026-02-23 10:43:58 +0530

    Time: 8 ms (97.78%), Space: 47.8 MB (98.02%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,20 +1,30 @@
 class Solution {
     public boolean hasAllCodes(String s, int k) {
         int n = s.length() ; 
-        if(n-k+1 < ( 1 << k )) return false ; 
+        boolean[] seen = new boolean[1<<k] ; 
+        int count = 0 ; 
+        if(n-k+1 < (1<<k)) return false ; 
 
-        // Take HashSet for store unique binary 
-        HashSet<String> set = new HashSet<>() ; 
-        int j = 0 ; 
+        int num = 0 ; 
+        for ( int i = 0 ; i < k ; i++ ) {
+            int bit = s.charAt(i) - '0' ; 
+            num = (num<<1) | bit ; 
+        }
+        seen[num] = true ; 
+        count++ ; 
 
-        // travel k ...... n 
-        
-        for ( int i = k ; i <= n ; i++ ) {
-            set.add(s.substring(j,i)) ; 
-            j++ ; 
 
-            // check set size == 2 ki power k 
-            if(set.size() == (1 << k )) return true ; 
+        for ( int i = k ; i < n ; i++ ) {
+            int bit = s.charAt(i) - '0' ; 
+            num = ((num << 1 ) | bit ) % ((1 << k) ) ; 
+
+            if(!seen[num]){
+                seen[num] = true ; 
+                count++ ; 
+            }
+
+            if(count == (1 << k )) return true ; 
+
         }
 
         return false ; 
