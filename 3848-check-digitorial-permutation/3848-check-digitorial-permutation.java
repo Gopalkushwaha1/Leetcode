1dc0ab8663562ebd9fd44e0be7061df30230824d
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2026-02-26 15:40:56 +0530

    Time: 2 ms (71.58%), Space: 43.1 MB (92.01%) - LeetHub

--- 3848-check-digitorial-permutation/3848-check-digitorial-permutation.java
+++ 3848-check-digitorial-permutation/3848-check-digitorial-permutation.java
@@ -1,29 +1,42 @@
 class Solution {
+    public boolean isSame(int sum , int n ) {
+        // take freq arr 
+        int[] sum1 = new int[10] ; 
+        int[] n1 = new int[10] ; 
 
-    private boolean isPermutation(int n, int target) {
-        int[] count = new int[10];
-
-        // Count digits of n
-        while (n > 0) {
-            count[n % 10]++;
-            n /= 10;
+        // fill array 
+        while ( sum > 0 ) {
+            sum1[sum%10]++ ; 
+            sum /= 10 ; 
         }
 
-        // Subtract digits of target
-        while (target > 0) {
-            count[target % 10]--;
-            target /= 10;
+        while ( n > 0 ) {
+            n1[n%10]++ ; 
+            n /= 10 ; 
         }
 
-        // Check if all counts are zero
-        for (int i = 0; i < 10; i++) {
-            if (count[i] != 0) return false;
+        // check by travel each freq 
+
+        for ( int i = 0 ; i < 10 ; i++ ) {
+            if(sum1[i] != n1[i]) return false ; 
         }
 
-        return true;
+        return true ; 
     }
-
     public boolean isDigitorialPermutation(int n) {
-        return isPermutation(n, 145) || isPermutation(n, 40585) || n <=2 ;
+        // take fact array sum and dummy num 
+
+        int num = n ; 
+        int sum = 0 ; 
+        int[] fact = new int[]{1,1,2,6,24,120,720,5040,40320,362880} ; 
+
+        while ( num > 0 ) {
+            int rem = num % 10 ; 
+            sum += fact[rem] ; 
+            num /= 10 ; 
+        }
+
+        // check equal 
+        return isSame(sum , n ) ; 
     }
 }
