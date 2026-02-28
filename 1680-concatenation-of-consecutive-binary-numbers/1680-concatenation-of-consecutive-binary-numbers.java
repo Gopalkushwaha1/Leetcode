048ce41b8d1d1b2f5915f83410f9d5d336e13b74
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2026-02-28 06:35:34 +0530

    Time: 161 ms (36.05%), Space: 46.3 MB (23.26%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -1,37 +1,17 @@
 class Solution {
-    static int ans = 0 ;
-    static int mod = 1_000_000_007 ;
-
-    public void changeBinaryToDecimal(String str ) {
-        // simple travel str bit 
-        // apply formula left side ans and add curr i bit 
-
-        for ( int i = 0 ; i < str.length() ; i++ ) {
-            // find curr bit 
-            int bit = str.charAt(i) - '0'; 
-
-            ans = ((ans * 2 ) + bit ) % mod  ; 
-        }
-    }
     public int concatenatedBinary(int n) {
-        // take long ans to store ans 
+        long ans = 0 ; 
+        int mod = 1_000_000_007 ; 
 
-        ans = 0 ; 
-        mod = 1_000_000_007 ; 
-
-        // travel i = 1 ......... n 
-
+        // treval i 1 ....n
         for ( int i = 1 ; i <= n ; i++ ) {
 
-            // find curr num binary 
+            // find curr length binary 
+            int len = Integer.toBinaryString(i).length() ; 
 
-            String binary = Integer.toBinaryString(i) ; 
-            // change binary to decimal add in ans ; 
-
-            changeBinaryToDecimal(binary) ; 
-
+            ans = ( (ans << len ) + i ) % mod ; 
         }
 
-        return (int) (ans%mod) ; 
+        return (int)ans ;  
     }
 }
