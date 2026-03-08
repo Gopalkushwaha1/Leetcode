ac9e716bbbc1dc6973247aa4b6b75c39dffc8778
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2026-03-08 12:23:50 +0530

    Time: 0 ms (100%), Space: 43 MB (87.9%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,41 +1,11 @@
 class Solution {
-    static String ans ; 
-    public void find(int n , StringBuilder sb , HashSet<String> set) {
-
-        // if ans change the stop 
-        if(ans.length() != 0 ) return ; 
-
-        // n == sb.length()  the check in hashset 
-        if(n == sb.length()) {
-            // if not found in HashSet ans mil gya 
-            if(! set.contains(sb.toString())) {
-                ans = sb.toString() ; 
-            }
-            return ; 
-        }
-
-        // add 1 and call 
-        sb.append("1") ; 
-        find(n , sb , set ) ; 
-        sb.deleteCharAt(sb.length()-1) ;   // backtrack 
-
-        // add 0 and call 
-        sb.append("0") ; 
-        find(n ,sb , set) ; 
-        sb.deleteCharAt(sb.length() - 1 ) ; 
-    }
     public String findDifferentBinaryString(String[] nums) {
-        ans = "" ; 
-        StringBuilder sb = new StringBuilder() ;      // to generate all string
-        HashSet<String> set = new HashSet<>() ; 
-
-        // fill hashset
-        for ( String s : nums ) {
-            set.add(s) ; 
+        StringBuilder ans = new StringBuilder();
+        
+        for (int i = 0; i < nums.length; i++) {
+            ans.append(nums[i].charAt(i) == '0' ? '1' : '0');
         }
-
-        // find unique that not present in nums 
-        find(nums.length , sb , set ) ; 
-        return ans ; 
+        
+        return ans.toString();
     }
 }
