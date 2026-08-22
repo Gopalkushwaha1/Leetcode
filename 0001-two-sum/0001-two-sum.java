fd7b8f1b2eeea070980de986e8aa2b1f04918afc
Gopal Kushwaha <161118852+Gopalkushwaha1@users.noreply.github.com>
2026-08-22 17:56:01 +0530

    Time: 4 ms (52.34%), Space: 47.3 MB (23.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,24 +1,26 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Create the ans 
+        // Create HashMap 
         int[] ans = new int[2] ; 
+        HashMap<Integer,Integer> map = new HashMap<>() ; 
         int n = nums.length ; 
-        // fix first value -> idx 
-        for ( int i = 0 ; i < n ; i++ ) {
-            int ele = nums[i] ; 
+        map.put(nums[0] , 0 ) ; 
+        // travel on nums check req sum --> map 
 
-            for ( int j = i + 1 ; j < n ; j++ ) {
-                // Check the condition
-                if ( ele + nums[j] == target ) {
-                    int index1 = i ; 
-                    int index2 = j ; 
+        for ( int i = 1 ; i <  n ; i++ ) {
+            int ele = nums[i]  ; 
+            int reqSum = target - ele; 
 
-                    ans[0] = index1 ; 
-                    ans[1] = index2 ; 
-                    break ; 
-                }
+            if ( map.containsKey(reqSum)) {
+                int idx1 = map.get(reqSum) ; 
+                int idx2 = i ; 
+
+                ans[0] = idx1 ; 
+                ans[1] = idx2 ; 
+
+                break ; 
             }
-
+            map.put(ele , i ) ; 
         }
         return ans ; 
     }
